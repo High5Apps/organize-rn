8ebdb452b10e15f3662989e6edbd4b0fe994f4b8
Julian Tigler <High5Apps@gmail.com>
2022-03-14 22:23:23 -0700

    Fix jest was broken by react navigation
    - Move jest config from package.json to jest.config.js
    - Add devDependencies on react-native-gesture-handler and react-native-reanimated
    - Mock react-native-gesture-handler and react-native-reanimated as recommended by https://reactnavigation.org/docs/testing

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,13 +1,10 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import { act, create } from 'react-test-renderer';
 import App from '../app/App';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  act(() => {
+    create(<App />);
+  });
 });
