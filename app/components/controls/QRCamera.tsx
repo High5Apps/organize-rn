b8a7dc5661960aae9abdac953997d8d56be52ecf
Julian Tigler <High5Apps@gmail.com>
2022-04-11 22:53:11 -0700

    Add QR code detection to QRCamera
    - Add dependency on vision-camera-code-scanner
    - Only activate camera when screen is focused
    - FrameProcessor runs once per second since the docs state:
    > A value of `1` indicates that the frame processor gets executed once per second, perfect for code scanning.
    - But consider increasing to 5 in the future since the docs also state:
    > For a QR Code Scanner, 5 FPS (200ms) might suffice
    - Known issues: Lines 23-25 of QRCamera.tsx sometimes cause the following error when
    navigating back from JoinOrg:
    ```
     ERROR  Warning: Can't perform a React state update on an unmounted component. This is a
    no-op, but it indicates a memory leak in your application. To fix, cancel all
    subscriptions and asynchronous tasks in a useEffect cleanup function.
        in QRCamera (at CameraControl.tsx:19)
     ```

--- app/components/controls/QRCamera.tsx
+++ app/components/controls/QRCamera.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import { useIsFocused } from '@react-navigation/native';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { Camera, useCameraDevices } from 'react-native-vision-camera';
+import { BarcodeFormat, useScanBarcodes } from 'vision-camera-code-scanner';
 import useTheme from '../../Theme';
 import FrameButton from './FrameButton';
 
@@ -18,17 +20,32 @@ const useStyles = () => {
 };
 
 export default function QRCamera() {
+  const [frameProcessor, barcodes] = useScanBarcodes([BarcodeFormat.QR_CODE], {
+    checkInverted: true,
+  });
+
+  useEffect(() => {
+    const contentData = barcodes.map((b) => b.content.data);
+    const contentDataSet = new Set(contentData);
+    const uniqueData = [...contentDataSet];
+    console.log({ uniqueData });
+  }, [barcodes]);
+
   const { styles } = useStyles();
 
   const devices = useCameraDevices();
   const device = devices.back;
+
+  const isFocused = useIsFocused();
 
   let content;
   if (device) {
     content = (
       <Camera
         device={device}
-        isActive
+        frameProcessor={frameProcessor}
+        frameProcessorFps={1}
+        isActive={isFocused}
         style={StyleSheet.absoluteFill}
       />
     );
--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     // Reanimated plugin must be listed last
-    'react-native-reanimated/plugin',
+    [
+      'react-native-reanimated/plugin',
+      {
+        globals: ['__scanCodes'],
+      },
+    ],
   ],
 };
