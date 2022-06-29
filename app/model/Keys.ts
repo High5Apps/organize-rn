956e19d0af409c2f3040e26591583c6117a6dc87
Julian Tigler <High5Apps@gmail.com>
2022-06-29 01:02:09 -0700

    Add Keys with RSA functions
    - Add dependency on react-native-rsa-native
    - https://github.com/amitaymolko/react-native-rsa-native

---
+++ app/model/Keys.ts
@@ -0,0 +1,28 @@
+import { RSAKeychain } from 'react-native-rsa-native';
+import { v4 as uuidv4 } from 'uuid';
+
+export default function Keys() {
+  return {
+    rsa: {
+      async create(bits: number) {
+        const publicKeyId = uuidv4();
+        const keys = await RSAKeychain.generateKeys(publicKeyId, bits);
+        return { publicKeyId, publicKey: keys.public };
+      },
+      async delete(publicKeyId: string) {
+        const succeeded = await RSAKeychain.deletePrivateKey(publicKeyId);
+        return succeeded;
+      },
+      async getPublicKey(publicKeyId: string) {
+        const publicKey = await RSAKeychain.getPublicKey(publicKeyId);
+        return publicKey;
+      },
+      async sign(
+        { publicKeyId, message }: { publicKeyId: string, message: string },
+      ) {
+        const messageSignature = await RSAKeychain.sign(message, publicKeyId);
+        return messageSignature;
+      },
+    },
+  };
+}
--- app/model/index.ts
+++ app/model/index.ts
@@ -2,6 +2,7 @@ export { default as ConfirmationAlert } from './ConfirmationAlert';
 export { default as useCurrentUser } from './CurrentUser';
 export { UserContextProvider, useUserContext } from './UserContext';
 export { default as FakeQRCodeValue } from './FakeQRCodeValue';
+export { default as Keys } from './Keys';
 export { default as NewOrgSteps } from './NewOrgSteps';
 export { default as QRCode, QR_CODE_TIME_TO_LIVE_MS } from './QRCode';
 export { default as Secret } from './Secret';
