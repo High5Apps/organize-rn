91d1976fc307c2a14335b4acc4eafa009261f8dd
Julian Tigler <High5Apps@gmail.com>
2022-07-07 01:07:03 -0700

    Add URL polyfill
    - https://github.com/charpeni/react-native-url-polyfill

--- index.js
+++ index.js
@@ -4,7 +4,11 @@
 
 // Reanimated must be the first import
 import 'react-native-reanimated';
+
+// Polyfills
 import 'react-native-get-random-values';
+import 'react-native-url-polyfill/auto';
+
 import { AppRegistry } from 'react-native';
 import App from './app/App';
 import { name as appName } from './app.json';
