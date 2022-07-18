21c937f1fb5422cd2d5db175a3e71ab092ae022a
Julian Tigler <High5Apps@gmail.com>
2022-07-17 23:05:01 -0700

    Refactor JWT to use Buffer for encoding conversions

--- app/model/JWT.ts
+++ app/model/JWT.ts
@@ -4,20 +4,15 @@ import { Buffer } from 'buffer';
 import { Signer } from './types';
 
 function base64ToBase64Url(base64: string) {
-  // https://en.wikipedia.org/wiki/Base64#URL_applications
-  const base64Url = (
-    base64
-      .split('+').join('-')
-      .split('/').join('_')
-      .split('=').join('')
-  );
+  const buffer = Buffer.from(base64, 'base64');
+  const base64Url = buffer.toString('base64url');
   return base64Url;
 }
 
 export function utf8ToBase64Url(utf8: string) {
   const buffer = Buffer.from(utf8);
-  const base64 = buffer.toString('base64');
-  return base64ToBase64Url(base64);
+  const base64 = buffer.toString('base64url');
+  return base64;
 }
 
 type Props = {
@@ -28,12 +23,12 @@ type Props = {
 
 // JWT Standard: https://www.rfc-editor.org/rfc/rfc7519.html
 export default function JWT({
-  expirationSecondsSinceEpoch: exp, signer, subject,
+  expirationSecondsSinceEpoch: exp, signer, subject: sub,
 }: Props) {
   async function toString() {
     const header = { alg: 'RS256', typ: 'JWT' };
 
-    const payload = { sub: subject, exp };
+    const payload = { sub, exp };
 
     const encodedHeader = utf8ToBase64Url(JSON.stringify(header));
     const encodedPayload = utf8ToBase64Url(JSON.stringify(payload));
