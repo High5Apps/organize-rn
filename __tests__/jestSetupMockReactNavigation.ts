High5Apps/organize-rn

commit 5758ae2f563b571b40f7ace4ef097ac12763fb0a

Use Stack instead of NativeStack for ConnectStack and WelcomeStack
- Stack is more customizable and handles all of the beforeRemove cases, so gestures can be re-enabled
  - https://reactnavigation.org/docs/stack-navigator/
- Fix SettingsButton margin
- Update react-native-gesture-handler from 2.3.1 to 2.6.0 to remove deprecation warning
  - https://github.com/software-mansion/react-native-gesture-handler/issues/1831
- Mock ReactNativeGestureHandler to fix failing test
- Ignore type issue in ReactNativeReanimated
- Fix broken tests by moving jestSetupMockReactNavigation from setupFiles to setupFilesAfterEnv
  - Recommended on https://docs.swmansion.com/react-native-reanimated/docs/guide/testing

jest.config.js
@@ -128,11 +128,13 @@ module.exports = {
   setupFiles: [
     "<rootDir>/__tests__/jestSetupMockAsyncStorage.ts",
     "<rootDir>/__tests__/jestSetupMockReactNativeVisionCamera.ts",
-    "<rootDir>/__tests__/jestSetupMockReactNavigation.ts"
+    "<rootDir>/node_modules/react-native-gesture-handler/jestSetup.js",
   ],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: [
+    "<rootDir>/__tests__/jestSetupMockReactNavigation.ts",
+  ],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
@@ -179,7 +181,7 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
-    "node_modules/(?!(@?react-native|@react-navigation))"
+    "node_modules/(?!(@?react-native|@react-navigation|react-native-reanimated))"
   ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them

app/navigation/ConnectStack.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import { ConnectScreen, NewConnectionScreen, SettingsScreen } from '../screens';
 import { ConnectStackParamList } from './types';
 import useDefaultStackNavigatorScreenOptions
   from './useDefaultStackNavigatorScreenOptions';
 
-const Stack = createNativeStackNavigator<ConnectStackParamList>();
+const Stack = createStackNavigator<ConnectStackParamList>();
 
 export default function ConnectStack() {
   const screenOptions = useDefaultStackNavigatorScreenOptions();
@@ -17,13 +17,7 @@ export default function ConnectStack() {
         component={NewConnectionScreen}
         name="NewConnection"
         options={{
-          // On iOS, the swipe down gesture is incompatible with the
-          // beforeRemove listener
-          gestureEnabled: false,
           presentation: 'modal',
-          // On Android, the header back button is incompatible with the
-          // beforeRemove listener
-          headerBackVisible: false,
           title: 'Create Connection',
         }}
       />

app/navigation/WelcomeStack.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import {
   WelcomeScreen, NewOrgScreen, OrgReview, JoinOrgScreen,
 } from '../screens';
 import type { WelcomeStackParamList } from './types';
 
-const Stack = createNativeStackNavigator<WelcomeStackParamList>();
+const Stack = createStackNavigator<WelcomeStackParamList>();
 
 export default function WelcomeStack() {
   return (
@@ -16,7 +16,7 @@ export default function WelcomeStack() {
       <Stack.Screen
         name="Welcome"
         component={WelcomeScreen}
-        options={{ animation: 'none' }}
+        options={{ animationEnabled: false }}
       />
       <Stack.Screen
         name="NewOrg"
@@ -27,8 +27,6 @@ export default function WelcomeStack() {
       <Stack.Screen
         name="JoinOrg"
         component={JoinOrgScreen}
-        // The swipe back gesture is incompatible with the beforeRemove listener
-        options={{ gestureEnabled: false }}
       />
     </Stack.Navigator>
   );

app/screens/ConnectScreen.tsx
@@ -33,6 +33,9 @@ const useStyles = () => {
     scrollView: {
       padding: spacing.m,
     },
+    settingsButton: {
+      marginEnd: spacing.m,
+    },
   });
 
   return { styles };
@@ -40,10 +43,12 @@ const useStyles = () => {
 
 function SettingsButton() {
   const navigation: SettingsScreenNavigationProp = useNavigation();
+  const { styles } = useStyles();
   return (
     <IconButton
       iconName="settings"
       onPress={() => navigation.navigate('Settings')}
+      style={styles.settingsButton}
     />
   );
 }

index.js
@@ -5,6 +5,9 @@
 // Reanimated must be the first import
 import 'react-native-reanimated';
 
+// Required by React Navigation Stack Navigator
+import 'react-native-gesture-handler';
+
 // Polyfills
 import 'react-native-get-random-values';
 import 'react-native-url-polyfill/auto';

commit 9ac814b212b757b0c04b65b82578e207ea52f29e

Fix Jest react-native-reanimated failure
- The issue was introduced in 5758ae2
  - https://github.com/High5Apps/organize/commit/5758ae2
- At the time, I though I fixed it by moving the setup from setupFiles to setupFilesAfterEnv
- This change adds a real fix of mocking the offending method
  - Suggested here: https://github.com/software-mansion/react-native-reanimated/issues/1380#issuecomment-865143328
- It also reverts the setupFiles/setupFilesAfterEnv change

jest.config.js
@@ -128,13 +128,12 @@ module.exports = {
   setupFiles: [
     "<rootDir>/__tests__/jestSetupMockAsyncStorage.ts",
     "<rootDir>/__tests__/jestSetupMockReactNativeVisionCamera.ts",
+    "<rootDir>/__tests__/jestSetupMockReactNavigation.ts",
     "<rootDir>/node_modules/react-native-gesture-handler/jestSetup.js",
   ],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: [
-    "<rootDir>/__tests__/jestSetupMockReactNavigation.ts",
-  ],
+  // setupFilesAfterEnv: [],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,

__tests__/jestSetupMockReactNavigation.ts
@@ -13,6 +13,11 @@ jest.mock('react-native-reanimated', () => {
   return Reanimated;
 });
 
+// https://github.com/software-mansion/react-native-reanimated/issues/1380#issuecomment-865143328
+// @ts-ignore
+// eslint-disable-next-line no-underscore-dangle
+global.__reanimatedWorkletInit = jest.fn();
+
 // Silence the warning: Animated: `useNativeDriver` is not supported because the
 // native animated module is missing
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');