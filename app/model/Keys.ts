64c0ec1918d4e9915972532aa92018ed336125df
Julian Tigler <High5Apps@gmail.com>
2023-03-28 11:53:49 +0300

    Remove unneeded RSA code and packages

--- app/model/Keys.ts
+++ app/model/Keys.ts
@@ -1,4 +1,3 @@
-import { RSAKeychain } from 'react-native-rsa-native';
 import { v4 as uuidv4 } from 'uuid';
 import ECCKeychain from './ECCKeychain';
 
@@ -22,27 +21,6 @@ export default function Keys() {
         { publicKeyId, message }: { publicKeyId: string, message: string },
       ) {
         const messageSignature = await ECCKeychain.sign(publicKeyId, message);
-        return messageSignature;
-      },
-    },
-    rsa: {
-      async create(bits: number) {
-        const publicKeyId = uuidv4();
-        const keys = await RSAKeychain.generateKeys(publicKeyId, bits);
-        return { publicKeyId, publicKey: keys.public };
-      },
-      async delete(publicKeyId: string) {
-        const succeeded = await RSAKeychain.deletePrivateKey(publicKeyId);
-        return succeeded;
-      },
-      async getPublicKey(publicKeyId: string) {
-        const publicKey = await RSAKeychain.getPublicKey(publicKeyId);
-        return publicKey;
-      },
-      async sign(
-        { publicKeyId, message }: { publicKeyId: string, message: string },
-      ) {
-        const messageSignature = await RSAKeychain.signWithAlgorithm(message, publicKeyId, 'SHA256withRSA');
         return messageSignature;
       },
     },
