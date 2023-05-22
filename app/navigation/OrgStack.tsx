2520d3c8f51ef251fbd6831efbb257de4a7a4cdf
Julian Tigler <High5Apps@gmail.com>
2023-05-22 22:23:38 +0300

    Set Org name as OrgScreen title

--- app/navigation/OrgStack.tsx
+++ app/navigation/OrgStack.tsx
@@ -3,15 +3,19 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { OrgScreen } from '../screens';
 import { OrgStackParamList } from './types';
 import useDefaultStackNavigatorScreenOptions from './useDefaultStackNavigatorScreenOptions';
+import { useUserContext } from '../model';
 
 const Stack = createNativeStackNavigator<OrgStackParamList>();
 
 export default function OrgStack() {
   const screenOptions = useDefaultStackNavigatorScreenOptions();
 
+  const { currentUser } = useUserContext();
+  const title = currentUser?.org?.name;
+
   return (
     <Stack.Navigator screenOptions={screenOptions}>
-      <Stack.Screen name="Org" component={OrgScreen} />
+      <Stack.Screen name="Org" component={OrgScreen} options={{ title }} />
     </Stack.Navigator>
   );
 }
