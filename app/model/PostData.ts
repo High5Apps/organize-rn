a877213a4ea731d653e94a9def0dc72dc1aaea47
Julian Tigler <High5Apps@gmail.com>
2023-08-15 16:15:15 -0700

    Add fetchOlderPosts to usePostData

--- app/model/PostData.ts
+++ app/model/PostData.ts
@@ -58,7 +58,38 @@ export default function usePostData() {
     // Need to reverse posts since they were fetched oldest first
     const reversedPosts = newerPostsWithoutDuplicates.reverse();
 
-    setPosts([...reversedPosts ?? [], ...posts]);
+    setPosts([...reversedPosts, ...posts]);
+  }
+
+  async function fetchNextOlderPosts() {
+    if (!isCurrentUserData(currentUser)) {
+      throw new Error('Expected currentUser to be set');
+    }
+
+    const jwt = await currentUser.createAuthToken({ scope: '*' });
+
+    const lastPost = posts.length > 0 ? posts.at(-1) : undefined;
+    const createdBefore = lastPost?.createdAt;
+    const { errorMessage, posts: fetchedPosts } = await fetchPosts({
+      createdBefore, jwt, sort: 'new',
+    });
+
+    if (errorMessage) {
+      throw new Error(errorMessage);
+    }
+
+    if (!fetchedPosts?.length) { return; }
+
+    // Due to millisecond rounding errors, it's possible that the previously
+    // oldest post could be included again in the returned posts. If needed, the
+    // following code filters it out.
+    const indexOfDuplicate = fetchedPosts.findIndex((post) => (
+      post.id === lastPost?.id
+    ));
+    const olderPostsWithoutDuplicates = (indexOfDuplicate === -1)
+      ? fetchedPosts : fetchedPosts.slice(1 + indexOfDuplicate);
+
+    setPosts([...posts, ...olderPostsWithoutDuplicates]);
   }
 
   useEffect(() => {
@@ -66,6 +97,6 @@ export default function usePostData() {
   }, []);
 
   return {
-    posts, fetchNewestPosts, fetchNextNewerPosts, ready,
+    posts, fetchNewestPosts, fetchNextNewerPosts, fetchNextOlderPosts, ready,
   };
 }
