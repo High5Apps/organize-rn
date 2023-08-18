7365ac52272d7bed0ad129afeb4ee70cdf01acef
Julian Tigler <High5Apps@gmail.com>
2023-08-18 11:52:49 -0700

    Add header and empty message to CommentList

--- app/components/controls/CommentList.tsx
+++ app/components/controls/CommentList.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, StyleSheet, Text } from 'react-native';
 import CommentRow from './CommentRow';
+import useTheme from '../../Theme';
+import { SectionHeader } from '../views';
+
+const useStyles = () => {
+  const { colors, font, spacing } = useTheme();
+
+  const styles = StyleSheet.create({
+    text: {
+      color: colors.labelSecondary,
+      fontSize: font.sizes.body,
+      fontFamily: font.weights.regular,
+      marginHorizontal: spacing.m,
+      marginVertical: spacing.s,
+      textAlign: 'center',
+    },
+  });
+
+  return { styles };
+};
 
 export default function CommentList() {
-  return <FlatList data={[]} renderItem={() => <CommentRow />} />;
+  const { styles } = useStyles();
+
+  return (
+    <FlatList
+      data={[]}
+      ListEmptyComponent={(
+        <Text style={styles.text}>Be the first to comment on this post</Text>
+      )}
+      ListHeaderComponent={<SectionHeader>Comments</SectionHeader>}
+      renderItem={() => <CommentRow />}
+    />
+  );
 }
