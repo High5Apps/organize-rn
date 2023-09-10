c831530ca3e7bee379501d0aa4a18c4727abf106
Julian Tigler <High5Apps@gmail.com>
2023-09-10 15:58:31 +0300

    Fix background sometimes flashed white in dark mode
    - Suggested by https://stackoverflow.com/a/68738911/2421313

--- app/navigation/Navigation.tsx
+++ app/navigation/Navigation.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import LinkingConfig from './LinkingConfig';
 import OrgTabs from './OrgTabs';
 import WelcomeStack from './WelcomeStack';
 import { useUserContext } from '../model';
+import useTheme from '../Theme';
 
 export default function Navigation() {
+  const { colors: { background } } = useTheme();
+  DefaultTheme.colors.background = background;
+
   const { currentUser } = useUserContext();
+
   return (
     <NavigationContainer linking={LinkingConfig}>
       {currentUser ? <OrgTabs /> : <WelcomeStack />}
