029e6705059acd04e89e5003cb5460d4448b3d4e
Julian Tigler <High5Apps@gmail.com>
2023-09-10 22:14:26 +0300

    Rename store to workplace in NewOrgStep

--- app/model/NewOrgSteps.ts
+++ app/model/NewOrgSteps.ts
@@ -25,7 +25,7 @@ const NewOrgSteps: NewOrgStep[] = [
     title: 'Name Your Org',
   },
   {
-    body: 'An Org is strongest and healthiest once it\'s recruited 70% of the people that fit its potential member definition.\n\nIf your definition is too broad, it\'ll be hard to hit 70%. If your definition is too strict, it\'ll be easier to hit 70%, but your Org may be too small to really benefit from "strength in numbers."\n\nThe most common definition is to include every employee at your store who isn\'t in management.',
+    body: 'An Org is strongest and healthiest once it\'s recruited 70% of the people that fit its potential member definition.\n\nIf your definition is too broad, it\'ll be hard to hit 70%. If your definition is too strict, it\'ll be easier to hit 70%, but your Org may be too small to really benefit from "strength in numbers."\n\nThe most common definition is to include every employee at your workplace who isn\'t in management.',
     headline: 'How should I define my potential members?',
     iconName: 'menu-book',
     maxLength: 75,
