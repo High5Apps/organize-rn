5e9ed1383c8d9a76b2c0a552306719cdec08ffb1
Julian Tigler <High5Apps@gmail.com>
2023-10-02 15:46:19 +0300

    Hide PostCategorySelector when category param is given
    - This simplifies NewPostScreen when the user has just pressed the FAB from General, Grievances, and Demands
    - PostCategorySelector is only shown when navigating from Recent, because there's no hint of which type of post the user wants to create
    - This also fixes a bug where NewPostScreen was assuming that it should add insertedPosts to the returnScreen param screen
    - This also prevents any confusion that could occur if a user created, say, a grievance from the demands screen, and then it wasn't shown in the list at all on goBack

--- app/screens/NewPostScreen.tsx
+++ app/screens/NewPostScreen.tsx
@@ -129,10 +129,12 @@ export default function NewPostScreen({
 
   return (
     <KeyboardAvoidingScreenBackground>
-      <PostCategorySelector
-        onSelectionChanged={setPostCategory}
-        selection={postCategory}
-      />
+      {!maybeCategory && (
+        <PostCategorySelector
+          onSelectionChanged={setPostCategory}
+          selection={postCategory}
+        />
+      )}
       <TextInputRow
         // Prevents dismissing the keyboard when hitting next on Android before
         // entering any input
