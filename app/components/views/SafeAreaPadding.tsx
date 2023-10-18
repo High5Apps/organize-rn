20332a3a6c5dc9ddcae2092f2e29f54e497bec3e
Julian Tigler <High5Apps@gmail.com>
2023-10-18 14:05:31 +0300

    Fix SafeAreaPadding background color
    - Previously it was fill, now it's background

--- app/components/views/SafeAreaPadding.tsx
+++ app/components/views/SafeAreaPadding.tsx
@@ -1,12 +1,15 @@
 import React, { PropsWithChildren } from 'react';
 import { View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import useTheme from '../../Theme';
 
 export default function SafeAreaPadding({ children }: PropsWithChildren<{}>) {
   const insets = useSafeAreaInsets();
+  const { colors } = useTheme();
   return (
     <View
       style={{
+        backgroundColor: colors.background,
         flex: 1,
 
         // Paddings to handle safe area
