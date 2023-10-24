ec0722075e82a0a2a6531f4f73f5fa6ee31bd349
Julian Tigler <High5Apps@gmail.com>
2023-10-24 15:00:50 +0300

    Modify Secret base64Url into base64

--- __tests__/model/Secret.test.ts
+++ __tests__/model/Secret.test.ts
@@ -1,5 +1,4 @@
 import { Secret } from '../../app/model';
-import { DEFAULT_SECRET_LENGTH_BYTES } from '../../app/model/Secret';
 
 function decodedByteCount(base64String: string) {
   const paddingLength = base64String.split('=').length - 1;
@@ -7,22 +6,17 @@ function decodedByteCount(base64String: string) {
 }
 
 describe('Secret', () => {
-  describe('base64Url', () => {
+  describe('base64', () => {
     it('includes only valid characters', () => {
-      const secret = Secret().base64Url();
-      expect(secret).toMatch(/^[a-zA-Z0-9_-]+={0,2}$/);
+      const secret = Secret().base64(32);
+      expect(secret).toMatch(/^[a-zA-Z0-9/+]+={0,2}$/);
     });
+  });
 
-    it('returns the correct number of bytes', () => {
-      [...Array(20).keys()].forEach((bytes) => {
-        const secret = Secret().base64Url(bytes);
-        expect(decodedByteCount(secret)).toEqual(bytes);
-      });
-    });
-
-    it('defaults to using DEFAULT_SECRET_LENGTH_BYTES', () => {
-      const secret = Secret().base64Url();
-      expect(decodedByteCount(secret)).toEqual(DEFAULT_SECRET_LENGTH_BYTES);
+  it('returns the correct number of bytes', () => {
+    [...Array(100).keys()].forEach((bytes) => {
+      const secret = Secret().base64(bytes);
+      expect(decodedByteCount(secret)).toEqual(bytes);
     });
   });
 });
--- app/model/Secret.ts
+++ app/model/Secret.ts
@@ -1,23 +1,9 @@
-import { Buffer } from 'buffer';
 import CryptoJS from 'crypto-js';
-
-// 64 bytes is the same length as `rails secret`
-export const DEFAULT_SECRET_LENGTH_BYTES = 64;
 
 export default function Secret() {
   return {
-    base64Url: (bytes: number = DEFAULT_SECRET_LENGTH_BYTES) => {
-      const randomByteArray = CryptoJS.lib.WordArray.random(4 * bytes);
-      const randomBuffer = Buffer.from(randomByteArray.words);
-      const randomBase64 = randomBuffer.toString('base64');
-
-      // https://en.wikipedia.org/wiki/Base64#URL_applications
-      const randomBase64Url = (
-        randomBase64
-          .split('+').join('-')
-          .split('/').join('_')
-      );
-      return randomBase64Url;
-    },
+    base64: (bytes: number) => (
+      CryptoJS.lib.WordArray.random(bytes).toString(CryptoJS.enc.Base64)
+    ),
   };
 }
