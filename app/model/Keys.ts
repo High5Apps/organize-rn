7ad6ebf8124506af7b4f9320c8d22ee3d1596145
Julian Tigler <High5Apps@gmail.com>
2023-10-24 12:20:01 +0300

    Add Keys.rsa with create, delete, encrypt, decrypt
    - 2048-bit RSA keys will be used to encrypt the AES 256-bit group key material
      - Since the group key needs to be shared from member to member, it must be stored outside of native key stores, which don't allow key export
      - However, the group key will also be imported into native key stores for encryption/decryption purposes
      - RSA keys are needed because Android doesn't yet support encrypting with ECC P256 directly, only signing/verifying are currently supported
        - https://stackoverflow.com/a/57634295/2421313
        - https://developer.android.com/reference/javax/crypto/Cipher
    - Re-add react-native-rsa-native@2.0.5
      - https://github.com/amitaymolko/react-native-rsa-native
      - Previously removed in https://github.com/High5Apps/organize-rn/commit/64c0ec1

--- app/model/Keys.ts
+++ app/model/Keys.ts
@@ -1,3 +1,4 @@
+import { RSAKeychain } from 'react-native-rsa-native';
 import { v4 as uuidv4 } from 'uuid';
 import ECCKeychain from './ECCKeychain';
 
@@ -17,11 +18,41 @@ export default function Keys() {
         const publicKey = await ECCKeychain.getPublicKey(publicKeyId);
         return publicKey;
       },
-      async sign(
-        { publicKeyId, message }: { publicKeyId: string, message: string },
-      ) {
+      async sign({
+        publicKeyId, message,
+      }: { publicKeyId: string; message: string; }) {
         const messageSignature = await ECCKeychain.sign(publicKeyId, message);
         return messageSignature;
+      },
+    },
+    rsa: {
+      async create() {
+        const publicKeyId = uuidv4();
+        await RSAKeychain.generateKeys(publicKeyId, 2048);
+        return { publicKeyId };
+      },
+      async delete(publicKeyId: string) {
+        const succeeded = await RSAKeychain.deletePrivateKey(publicKeyId);
+        return succeeded;
+      },
+      async encrypt({
+        publicKeyId, message,
+      }: { publicKeyId: string; message: string; }) {
+        const encryptedMessage = await RSAKeychain.encrypt(
+          message,
+          publicKeyId,
+        );
+        const withoutWhitespace = encryptedMessage.replace(/\s/g, '');
+        return { base64EncodedEncryptedMessage: withoutWhitespace };
+      },
+      async decrypt({
+        base64EncodedEncryptedMessage, publicKeyId,
+      }: { publicKeyId: string; base64EncodedEncryptedMessage: string; }) {
+        const message = await RSAKeychain.decrypt(
+          base64EncodedEncryptedMessage,
+          publicKeyId,
+        );
+        return { message };
       },
     },
   };
