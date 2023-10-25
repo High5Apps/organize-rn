8ba7da9aecd80b521c3504e1d481fba7d75f89ad
Julian Tigler <High5Apps@gmail.com>
2023-10-25 11:50:34 +0300

    Remove stale documentation in Routes
    - `npm run tether:android` also works for the emulator
    - There's no need to change the origin anymore

--- app/networking/Routes.ts
+++ app/networking/Routes.ts
@@ -1,10 +1,6 @@
 // To hit a local backend development server over USB, npm run tether:android
 // It is not currently easy to hit a local dev server over USB on iOS
 
-// To hit a local backend development server from the emulator, change the
-// origin below to use 'http://10.0.2.2:3000'. For more info, see
-// https://developer.android.com/studio/run/emulator-networking#networkaddresses
-
 const origin = __DEV__ ? 'http://localhost:3000' : 'https://getorganize.app';
 
 const version = 'v1';
