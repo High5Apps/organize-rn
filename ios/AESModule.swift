00d7a9ae8416335d7db2db8f1d4a298b00c8e329
Julian Tigler <High5Apps@gmail.com>
2023-10-30 15:23:10 +0200

    Add iOS AESModule.encrypt
    - Refactor RSAModule.swift to expose a static decrypt function
    - https://dev.to/craftzdog/how-to-encrypt-decrypt-with-aes-gcm-using-cryptokit-in-swift-24h1
    - https://developer.apple.com/documentation/cryptokit/symmetrickey

--- ios/AESModule.swift
+++ ios/AESModule.swift
@@ -6,17 +6,49 @@
 //
 
 import Foundation
+import CryptoKit
 
 @objc(AESModule)
 class AESModule: NSObject {
+  let AES_ERROR_CODE = "E_AES"
+  let RETURN_KEY_ENCRYPTED_MESSAGE = "base64EncryptedMessage"
+  let RETURN_KEY_INITIALIZATION_VECTOR = "base64InitializationVector"
+  let RETURN_KEY_INTEGRITY_CHECK = "base64IntegrityCheck"
 
   @objc
   static func requiresMainQueueSetup() -> Bool { false }
 
   @objc
   func encrypt(_ wrappedKey: String, wrapperKeyId: String, message: String, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
-    print("wrappedKey: \(wrappedKey)")
-    print("wrapperKeyId: \(wrapperKeyId)")
-    resolve(message)
+    guard let symmetricKeyBase64 = try? RSAModule.decrypt(wrapperKeyId, base64EncryptedMessage: wrappedKey) else {
+      reject(AES_ERROR_CODE, "Failed to decrypt wrapped key", nil)
+      return
+    }
+    
+    guard let symmetricKeyData = Data(base64Encoded: symmetricKeyBase64) else {
+      reject(AES_ERROR_CODE, "Failed to decode base64EncryptedMessage into data", nil)
+      return
+    }
+    
+    guard let messageData = message.data(using: .utf8) else {
+      reject(AES_ERROR_CODE, "Failed to convert message into UTF-8 data", nil)
+      return
+    }
+
+    let symmetricKey = SymmetricKey(data: symmetricKeyData)
+    guard let encryptedMessageData = try? AES.GCM.seal(messageData, using: symmetricKey) else {
+      reject(AES_ERROR_CODE, "Failed to encrypt data", nil)
+      return
+    }
+    
+    let ciphertext = encryptedMessageData.ciphertext.base64EncodedString()
+    let initializationVector = encryptedMessageData.nonce.dataRepresentation.base64EncodedString()
+    let integrityCheck = encryptedMessageData.tag.base64EncodedString()
+    
+    resolve([
+      RETURN_KEY_ENCRYPTED_MESSAGE: ciphertext,
+      RETURN_KEY_INITIALIZATION_VECTOR: initializationVector,
+      RETURN_KEY_INTEGRITY_CHECK: integrityCheck,
+    ]);
   }
 }
--- ios/RSAModule.swift
+++ ios/RSAModule.swift
@@ -9,9 +9,10 @@ import Foundation
 
 @objc(RSAModule)
 class RSAModule: NSObject {
-  let KEY_SIZE = 2048
+  static let KEY_SIZE = 2048
+  static let RSA_ALGORITHM: SecKeyAlgorithm = .rsaEncryptionOAEPSHA256
+
   let MIN_PLAIN_TEXT_TO_BLOCK_SIZE_DIFFERENCE_BYTES = 130
-  let RSA_ALGORITHM: SecKeyAlgorithm = .rsaEncryptionOAEPSHA256
   let RSA_ERROR_CODE = "E_RSA"
 
   @objc
@@ -19,30 +20,12 @@ class RSAModule: NSObject {
   
   @objc
   func decrypt(_ publicKeyId: String, base64EncryptedMessage: String, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
-    guard let privateKey = try? getPrivateKey(publicKeyId) else {
-      reject(RSA_ERROR_CODE, "Failed to fetch RSA private key", nil)
-      return
-    }
+    let message: String
 
-    guard SecKeyIsAlgorithmSupported(privateKey, .decrypt, RSA_ALGORITHM) else {
-      reject(RSA_ERROR_CODE, "RSA decryption algorithm not supported", nil)
-      return
-    }
-
-    guard let cipherTextData = Data(base64Encoded: base64EncryptedMessage), cipherTextData.count == SecKeyGetBlockSize(privateKey) else {
-      reject(RSA_ERROR_CODE, "Failed to decode base64EncryptedMessage into data", nil)
-      return
-    }
-
-    var unmanagedError: Unmanaged<CFError>?
-    guard let messageData = SecKeyCreateDecryptedData(privateKey, RSA_ALGORITHM, cipherTextData as CFData, &unmanagedError) as Data? else {
-      let error = unmanagedError!.takeRetainedValue() as Error
+    do {
+      message = try Self.decrypt(publicKeyId, base64EncryptedMessage: base64EncryptedMessage)
+    } catch {
       reject(RSA_ERROR_CODE, "Failed to decrypt message", error)
-      return
-    }
-    
-    guard let message = String(data: messageData, encoding: .utf8) else {
-      reject(RSA_ERROR_CODE, "Failed to encode message data into UTF-8", nil)
       return
     }
 
@@ -50,8 +33,36 @@ class RSAModule: NSObject {
   }
   
   @objc
+  static func decrypt(_ publicKeyId: String, base64EncryptedMessage: String) throws -> String {
+    guard let privateKey = try? getPrivateKey(publicKeyId) else {
+      throw RSAError.runtimeError("Failed to fetch RSA private key")
+    }
+
+    guard SecKeyIsAlgorithmSupported(privateKey, .decrypt, RSA_ALGORITHM) else {
+      throw RSAError.runtimeError("RSA decryption algorithm not supported")
+    }
+
+    guard let cipherTextData = Data(base64Encoded: base64EncryptedMessage), cipherTextData.count == SecKeyGetBlockSize(privateKey) else {
+      throw RSAError.runtimeError("Failed to decode base64EncryptedMessage into data")
+    }
+
+    var unmanagedError: Unmanaged<CFError>?
+    guard let messageData = SecKeyCreateDecryptedData(privateKey, RSA_ALGORITHM, cipherTextData as CFData, &unmanagedError) as Data? else {
+      let error = unmanagedError!.takeRetainedValue() as Error
+      print(error)
+      throw RSAError.runtimeError("Failed to decrypt message")
+    }
+    
+    guard let message = String(data: messageData, encoding: .utf8) else {
+      throw RSAError.runtimeError("Failed to encode message data into UTF-8")
+    }
+    
+    return message
+  }
+  
+  @objc
   func deletePrivateKey(_ publicKeyId: String, resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) -> Void {
-    guard let attributes = try? queryAttributes(publicKeyId) else {
+    guard let attributes = try? Self.queryAttributes(publicKeyId) else {
       reject(RSA_ERROR_CODE, "Failed to create query attributes", nil)
       return
     }
@@ -70,7 +81,7 @@ class RSAModule: NSObject {
   
   @objc
   func encrypt(_ publicKeyId: String, message: String, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
-    guard let privateKey = try? getPrivateKey(publicKeyId) else {
+    guard let privateKey = try? Self.getPrivateKey(publicKeyId) else {
       reject(RSA_ERROR_CODE, "Failed to fetch RSA private key", nil)
       return
     }
@@ -80,7 +91,7 @@ class RSAModule: NSObject {
       return
     }
 
-    guard SecKeyIsAlgorithmSupported(publicKey, .encrypt, RSA_ALGORITHM) else {
+    guard SecKeyIsAlgorithmSupported(publicKey, .encrypt, Self.RSA_ALGORITHM) else {
       reject(RSA_ERROR_CODE, "RSA encryption algorithm not supported", nil)
       return
     }
@@ -96,7 +107,7 @@ class RSAModule: NSObject {
     }
 
     var unmanagedError: Unmanaged<CFError>?
-    guard let cipherText = SecKeyCreateEncryptedData(publicKey, RSA_ALGORITHM, messageData as CFData, &unmanagedError) as Data? else {
+    guard let cipherText = SecKeyCreateEncryptedData(publicKey, Self.RSA_ALGORITHM, messageData as CFData, &unmanagedError) as Data? else {
       let error = unmanagedError!.takeRetainedValue() as Error
       reject(RSA_ERROR_CODE, "Failed to encrypt message", error)
       return
@@ -115,7 +126,7 @@ class RSAModule: NSObject {
     let attributes: [String: Any] = [
       kSecClass as String: kSecClassKey,
       kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
-      kSecAttrKeySizeInBits as String: KEY_SIZE,
+      kSecAttrKeySizeInBits as String: Self.KEY_SIZE,
       kSecPrivateKeyAttrs as String: [
         kSecAttrIsPermanent as String: true,
         kSecAttrApplicationTag as String: tag
@@ -132,7 +143,7 @@ class RSAModule: NSObject {
     resolve(nil)
   }
   
-  private func queryAttributes(_ publicKeyId: String) throws -> [String: Any] {
+  private static func queryAttributes(_ publicKeyId: String) throws -> [String: Any] {
     guard let tag = publicKeyId.data(using: .utf8) else {
       throw RSAError.runtimeError("Failed to convert tag into UTF-8 data")
     }
@@ -148,7 +159,7 @@ class RSAModule: NSObject {
     ]
   }
   
-  private func getPrivateKey(_ publicKeyId: String) throws -> SecKey {
+  private static func getPrivateKey(_ publicKeyId: String) throws -> SecKey {
     guard let attributes = try? queryAttributes(publicKeyId) else {
       throw RSAError.runtimeError("Failed to create query attributes")
     }
