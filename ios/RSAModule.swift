112b3fc1e7d7b2a09d28ac73ad659acb2c0f9246
Julian Tigler <High5Apps@gmail.com>
2023-10-30 09:08:34 +0200

    Implement iOS RSAModule.generateKeys
    - CryptoKit doesn't support RSA, need to use the legacy SecKey APIs
      - https://developer.apple.com/forums/thread/687335
    - Creating an Asymmetric Key Pair
      - https://developer.apple.com/documentation/security/certificate_key_and_trust_services/keys/generating_new_cryptographic_keys#2863927
    - kSecAttrKeyType options
      - https://developer.apple.com/documentation/security/keychain_services/keychain_items/item_attribute_keys_and_values#1679067

--- ios/RSAModule.swift
+++ ios/RSAModule.swift
@@ -9,12 +9,36 @@ import Foundation
 
 @objc(RSAModule)
 class RSAModule: NSObject {
+  let KEY_SIZE = 2048
+  let RSA_ERROR_CODE = "E_RSA"
 
   @objc
   static func requiresMainQueueSetup() -> Bool { false }
 
   @objc
-  func generateKeys(_ publicKeyId: String, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
-    resolve(publicKeyId)
+  func generateKeys(_ publicKeyId: String, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {    
+    guard let tag = publicKeyId.data(using: .utf8) else {
+      reject(RSA_ERROR_CODE, "Failed to convert tag into UTF-8 data", nil)
+      return
+    }
+    
+    let attributes: [String: Any] = [
+      kSecClass as String: kSecClassKey,
+      kSecAttrKeyType as String: kSecAttrKeyTypeRSA,
+      kSecAttrKeySizeInBits as String: KEY_SIZE,
+      kSecPrivateKeyAttrs as String: [
+        kSecAttrIsPermanent as String: true,
+        kSecAttrApplicationTag as String: tag
+      ]
+    ]
+    
+    var unmanagedError: Unmanaged<CFError>?
+    guard SecKeyCreateRandomKey(attributes as CFDictionary, &unmanagedError) != nil else {
+      let error = unmanagedError!.takeRetainedValue() as Error
+      reject(RSA_ERROR_CODE, "Failed to store RSA PrivateKey in Keychain", error)
+      return
+    }
+
+    resolve(nil)
   }
 }
