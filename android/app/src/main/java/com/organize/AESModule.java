7bde324a6b0b43bc6f928895fc1b81dc1c9d9651
Julian Tigler <High5Apps@gmail.com>
2023-10-31 09:37:46 +0200

    Add Android AESModule.decrypt
    - Kotlin AES GCM example code
      - https://levelup.gitconnected.com/doing-aes-gcm-in-android-adventures-in-the-field-72617401269d
    - Concatenate byte[]
      - https://stackoverflow.com/a/5513188/2421313

--- android/app/src/main/java/com/organize/AESModule.java
+++ android/app/src/main/java/com/organize/AESModule.java
@@ -3,6 +3,7 @@ package com.organize;
 import static com.organize.CommonCrypto.fromBase64;
 import static com.organize.CommonCrypto.fromUtf8;
 import static com.organize.CommonCrypto.toBase64;
+import static com.organize.CommonCrypto.toUtf8;
 
 import android.util.Log;
 
@@ -15,8 +16,10 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.WritableNativeMap;
 
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.AlgorithmParameterSpec;
 import java.util.Arrays;
 
 import javax.crypto.BadPaddingException;
@@ -24,6 +27,7 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class AESModule extends ReactContextBaseJavaModule {
@@ -44,6 +48,43 @@ public class AESModule extends ReactContextBaseJavaModule {
     @Override
     public String getName() {
         return MODULE_NAME;
+    }
+
+    @ReactMethod
+    public void decrypt(
+            String wrappedKey,
+            String wrapperKeyId,
+            String base64EncryptedMessage,
+            String base64InitializationVector,
+            String base64IntegrityCheck,
+            Promise promise) {
+        byte[] initializationVector = fromBase64(base64InitializationVector);
+        GCMParameterSpec gcmSpec =
+                new GCMParameterSpec(INTEGRITY_CHECK_LENGTH_BYTES * 8, initializationVector);
+
+        Cipher cipher;
+        try {
+            cipher = getCipher(Cipher.DECRYPT_MODE, wrappedKey, wrapperKeyId, gcmSpec);
+        } catch (AESException e) {
+            Log.e(getName(), e.toString());
+            promise.reject(e);
+            return;
+        }
+
+        byte[] ciphertext = fromBase64(base64EncryptedMessage);
+        byte[] integrityCheck = fromBase64(base64IntegrityCheck);
+        byte[] ciphertextAndIntegrityCheck = concatenate(ciphertext, integrityCheck);
+
+        byte[] message;
+        try {
+            message = cipher.doFinal(ciphertextAndIntegrityCheck);
+        } catch (BadPaddingException | IllegalBlockSizeException e) {
+            Log.e(getName(), e.toString());
+            promise.reject(e);
+            return;
+        }
+
+        promise.resolve(toUtf8(message));
     }
 
     @ReactMethod
@@ -81,7 +122,11 @@ public class AESModule extends ReactContextBaseJavaModule {
         promise.resolve(resultMap);
     }
 
-    private Cipher getCipher(int operationalMode, String wrappedKey, String wrapperKeyId)
+    private Cipher getCipher(
+            int operationalMode,
+            String wrappedKey,
+            String wrapperKeyId,
+            AlgorithmParameterSpec maybeSpec)
             throws AESException {
         String symmetricKeyBase64;
         try {
@@ -96,13 +141,28 @@ public class AESModule extends ReactContextBaseJavaModule {
         Cipher cipher;
         try {
             cipher = Cipher.getInstance(CIPHER_TRANSFORMATION_NAME);
-            cipher.init(operationalMode, secretKey);
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e) {
+            cipher.init(operationalMode, secretKey, maybeSpec);
+        } catch (NoSuchAlgorithmException
+                 | NoSuchPaddingException
+                 | InvalidKeyException
+                 | InvalidAlgorithmParameterException e) {
             Log.e(getName(), e.toString());
             throw new AESException("Failed to initialize AES cipher");
         }
 
         return cipher;
+    }
+
+    private Cipher getCipher(int operationalMode, String wrappedKey, String wrapperKeyId)
+            throws AESException {
+        return getCipher(operationalMode, wrappedKey, wrapperKeyId, null);
+    }
+
+    private byte[] concatenate(byte[] a, byte[] b) {
+        byte[] result = new byte[a.length + b.length];
+        System.arraycopy(a, 0, result, 0, a.length);
+        System.arraycopy(b, 0, result, a.length, b.length);
+        return result;
     }
 
     private static class AESException extends Exception {
