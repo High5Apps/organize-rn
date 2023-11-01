ac7c7f4ace384c684ba1a20b613fab32e3214bda
Julian Tigler <High5Apps@gmail.com>
2023-11-01 10:22:11 +0200

    Hide developer settings unless ENABLE_DEVELOPER_SETTINGS is set

--- app/model/Settings.ts
+++ app/model/Settings.ts
@@ -1,4 +1,6 @@
+import { useMemo } from 'react';
 import { Share } from 'react-native';
+import { ENABLE_DEVELOPER_SETTINGS } from './Config';
 import ConfirmationAlert from './ConfirmationAlert';
 import { useUserContext } from './UserContext';
 import { isCurrentUserData, type SettingsSection } from './types';
@@ -11,33 +13,40 @@ export default function useSettings(): SettingsSection[] {
     return [];
   }
 
-  return [
-    {
-      title: 'Org',
-      data: [
-        {
-          iconName: 'delete',
-          onPress: ConfirmationAlert({
-            destructiveAction: 'Leave Org',
-            destructiveActionInTitle: 'leave this Org',
-            onConfirm: logOut,
-          }).show,
-          title: 'Leave Org',
-        },
-      ],
-    },
-    {
-      title: 'Developer',
-      data: [
-        {
-          iconName: 'vpn-key',
-          onPress: async () => {
-            const groupKey = await currentUser.decryptGroupKey();
-            Share.share({ message: groupKey });
+  return useMemo(() => {
+    const settings = [
+      {
+        title: 'Org',
+        data: [
+          {
+            iconName: 'delete',
+            onPress: ConfirmationAlert({
+              destructiveAction: 'Leave Org',
+              destructiveActionInTitle: 'leave this Org',
+              onConfirm: logOut,
+            }).show,
+            title: 'Leave Org',
           },
-          title: 'Share Group Key',
-        },
-      ],
-    },
-  ];
+        ],
+      },
+    ];
+
+    if (ENABLE_DEVELOPER_SETTINGS) {
+      settings.push({
+        title: 'Developer',
+        data: [
+          {
+            iconName: 'vpn-key',
+            onPress: async () => {
+              const groupKey = await currentUser.decryptGroupKey();
+              Share.share({ message: groupKey });
+            },
+            title: 'Share Group Key',
+          },
+        ],
+      });
+    }
+
+    return settings;
+  }, [currentUser, logOut, ENABLE_DEVELOPER_SETTINGS]);
 }
