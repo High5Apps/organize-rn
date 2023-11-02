a23ffa0a181f4c17633fb85bea7fb0c7bcb690b3
Julian Tigler <High5Apps@gmail.com>
2023-11-02 09:16:30 +0200

    Extract decrypt(_:symmetricKey:) from decrypt
    - Create EncryptedMessage struct

--- ios/AESModule.swift
+++ ios/AESModule.swift
@@ -20,43 +20,18 @@ class AESModule: NSObject {
 
   @objc
   func decrypt(_ wrappedKey: String, wrapperKeyId: String, base64EncryptedMessage: String, base64InitializationVector: String, base64IntegrityCheck: String, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) -> Void {
-    guard let ciphertextData = Data(base64Encoded: base64EncryptedMessage) else {
-      reject(AES_ERROR_CODE, "Failed to decode base64EncryptedMessage into data", nil)
-      return
-    }
-    
-    guard let nonceData = Data(base64Encoded: base64InitializationVector) else {
-      reject(AES_ERROR_CODE, "Failed to decode base64InitializationVector", nil)
-      return
-    }
-    
-    guard let nonce = try? AES.GCM.Nonce(data: nonceData) else {
-      reject(AES_ERROR_CODE, "Failed to parse nonce from base64InitializationVector", nil)
-      return
-    }
-    
-    guard let tagData = Data(base64Encoded: base64IntegrityCheck) else {
-      reject(AES_ERROR_CODE, "Failed to decode base64IntegrityCheck", nil)
-      return
-    }
-    
-    guard let sealedBox = try? AES.GCM.SealedBox(nonce: nonce, ciphertext: ciphertextData, tag: tagData) else {
-      reject(AES_ERROR_CODE, "Failed to create SealedBox", nil)
-      return
-    }
-    
     guard let symmetricKey = try? getSymmetricKey(wrappedKey, wrapperKeyId) else {
       reject(AES_ERROR_CODE, "Failed to unwrap symmetric key", nil)
       return
     }
     
-    guard let messageData = try? AES.GCM.open(sealedBox, using: symmetricKey) else {
-      reject(AES_ERROR_CODE, "Failed to open SealedBox data", nil)
-      return
-    }
-
-    guard let message = String(data: messageData, encoding: .utf8) else {
-      reject(AES_ERROR_CODE, "Failed to encode message data to UTF-8", nil)
+    let encryptedMessage = EncryptedMessage(base64EncryptedMessage, base64InitializationVector, base64IntegrityCheck)
+    
+    let message: String
+    do {
+      message = try decrypt(encryptedMessage, with: symmetricKey)
+    } catch {
+      reject(AES_ERROR_CODE, error.localizedDescription, error)
       return
     }
     
@@ -103,7 +78,51 @@ class AESModule: NSObject {
     return SymmetricKey(data: symmetricKeyData)
   }
   
+  private func decrypt(_ message: EncryptedMessage, with symmetricKey: SymmetricKey) throws -> String {
+    guard let ciphertextData = Data(base64Encoded: message.base64EncryptedMessage) else {
+      throw AESError.runtimeError("Failed to decode base64EncryptedMessage into data")
+    }
+    
+    guard let nonceData = Data(base64Encoded: message.base64InitializationVector) else {
+      throw AESError.runtimeError("Failed to decode base64InitializationVector")
+    }
+    
+    guard let nonce = try? AES.GCM.Nonce(data: nonceData) else {
+      throw AESError.runtimeError("Failed to parse nonce from base64InitializationVector")
+    }
+    
+    guard let tagData = Data(base64Encoded: message.base64IntegrityCheck) else {
+      throw AESError.runtimeError("Failed to decode base64IntegrityCheck")
+    }
+    
+    guard let sealedBox = try? AES.GCM.SealedBox(nonce: nonce, ciphertext: ciphertextData, tag: tagData) else {
+      throw AESError.runtimeError("Failed to create SealedBox")
+    }
+    
+    guard let messageData = try? AES.GCM.open(sealedBox, using: symmetricKey) else {
+      throw AESError.runtimeError("Failed to open SealedBox data")
+    }
+
+    guard let message = String(data: messageData, encoding: .utf8) else {
+      throw AESError.runtimeError("Failed to encode message data to UTF-8")
+    }
+    
+    return message
+  }
+  
   enum AESError: Error {
       case runtimeError(String)
+  }
+  
+  private struct EncryptedMessage: Codable {
+    let base64EncryptedMessage: String
+    let base64InitializationVector: String
+    let base64IntegrityCheck: String
+    
+    init(_ base64EncryptedMessage: String, _ base64InitializationVector: String, _ base64IntegrityCheck: String) {
+      self.base64EncryptedMessage = base64EncryptedMessage
+      self.base64InitializationVector = base64InitializationVector
+      self.base64IntegrityCheck = base64IntegrityCheck
+    }
   }
 }
