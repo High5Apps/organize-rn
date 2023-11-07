84df6ca2a5bab6380b0cc03d3df4715d77001547
Julian Tigler <High5Apps@gmail.com>
2023-11-07 09:36:43 +0200

    Add New Vote primary button to VoteScreen

--- app/screens/BallotsScreen.tsx
+++ app/screens/BallotsScreen.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
-import type { BallotsScreenProps } from '../navigation';
-import PlaceholderScreen from './PlaceholderScreen';
+import { StyleSheet } from 'react-native';
+import { PrimaryButton, ScreenBackground } from '../components';
+import useTheme from '../Theme';
 
-export default function BallotsScreen({ route }: BallotsScreenProps) {
-  const { name } = route;
-  return <PlaceholderScreen name={name} />;
+const useStyles = () => {
+  const { sizes, spacing } = useTheme();
+
+  const styles = StyleSheet.create({
+    button: {
+      bottom: spacing.m,
+      end: spacing.m,
+      height: sizes.buttonHeight,
+      paddingHorizontal: spacing.m,
+      position: 'absolute',
+    },
+  });
+
+  return { styles };
+};
+
+export default function VoteScreen() {
+  const { styles } = useStyles();
+  return (
+    <ScreenBackground>
+      <PrimaryButton
+        iconName="add"
+        label="New Vote"
+        onPress={() => console.log('press')}
+        style={styles.button}
+      />
+    </ScreenBackground>
+  );
 }
