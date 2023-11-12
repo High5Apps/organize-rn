b7e85c74747da8364cc2d38baa04e75b55801c34
Julian Tigler <High5Apps@gmail.com>
2023-11-12 17:16:59 +0200

    Hook up NewYesNoBallotScreen Publish FAB to the backend

--- app/screens/NewYesNoBallotScreen.tsx
+++ app/screens/NewYesNoBallotScreen.tsx
@@ -1,21 +1,34 @@
 import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import {
   DateTimeSelector, HeaderText, KeyboardAvoidingScreenBackground,
-  MultilineTextInput,
+  MultilineTextInput, PrimaryButton, useRequestProgress,
 } from '../components';
-import { useCachedValue } from '../model';
+import {
+  GENERIC_ERROR_MESSAGE, isCurrentUserData, useCachedValue, useUserContext,
+} from '../model';
 import useTheme from '../Theme';
+import { createBallot } from '../networking';
+import type { NewYesNoBallotScreenProps } from '../navigation';
 
 const CACHE_KEY_QUESTION = 'newYesNoVoteQuestion';
 const MAX_QUESTION_LENGTH = 120;
+const CANDIDATE_TITLES = ['Yes', 'No'];
+const BALLOT_CATEGORY = 'yes_no';
 
 const useStyles = () => {
-  const { spacing } = useTheme();
+  const { sizes, spacing } = useTheme();
 
   const styles = StyleSheet.create({
+    button: {
+      alignSelf: 'flex-end',
+      flex: 0,
+      height: sizes.buttonHeight,
+      paddingHorizontal: spacing.m,
+    },
     container: {
-      padding: spacing.m,
+      flex: 1,
+      margin: spacing.m,
       rowGap: spacing.s,
     },
     dateTimeSelector: {
@@ -24,6 +37,9 @@ const useStyles = () => {
     multilineTextInput: {
       marginBottom: spacing.s,
       maxHeight: 100,
+    },
+    spacer: {
+      flex: 1,
     },
   });
 
@@ -38,11 +54,57 @@ function startOfNextHourIn7Days(): Date {
   return nextWeek;
 }
 
-export default function NewYesNoBallotScreen() {
+export default function NewYesNoBallotScreen({
+  navigation,
+}: NewYesNoBallotScreenProps) {
   const [question, setQuestion] = useCachedValue<string>(CACHE_KEY_QUESTION);
   const [votingEnd, setVotingEnd] = useState(startOfNextHourIn7Days());
 
+  const { currentUser } = useUserContext();
+
   const { styles } = useStyles();
+
+  const { RequestProgress, setLoading, setResult } = useRequestProgress();
+
+  const onPublishPressed = async () => {
+    if (!isCurrentUserData(currentUser)) { return; }
+
+    setLoading(true);
+    setResult('none');
+
+    const strippedQuestion = question?.trim() ?? '';
+    setQuestion(strippedQuestion);
+
+    let errorMessage: string | undefined;
+    try {
+      const jwt = await currentUser.createAuthToken({ scope: '*' });
+      const { e2eEncrypt, e2eEncryptMany } = currentUser;
+
+      const { errorMessage: maybeErrorMessage } = await createBallot({
+        candidateTitles: CANDIDATE_TITLES,
+        category: BALLOT_CATEGORY,
+        e2eEncrypt,
+        e2eEncryptMany,
+        jwt,
+        question: strippedQuestion,
+        votingEndsAt: votingEnd,
+      });
+
+      if (maybeErrorMessage !== undefined) {
+        errorMessage = maybeErrorMessage;
+      }
+    } catch (error) {
+      console.error(error);
+      errorMessage = GENERIC_ERROR_MESSAGE;
+    }
+
+    if (errorMessage) {
+      setResult('error', { message: errorMessage });
+    } else {
+      setResult('success', { message: 'Successfully created yes/no vote' });
+      navigation.navigate('Ballots');
+    }
+  };
 
   return (
     <KeyboardAvoidingScreenBackground style={styles.container}>
@@ -62,6 +124,14 @@ export default function NewYesNoBallotScreen() {
         dateTime={votingEnd}
         setDateTime={setVotingEnd}
         style={styles.dateTimeSelector}
+      />
+      <View style={styles.spacer} />
+      <RequestProgress />
+      <PrimaryButton
+        iconName="publish"
+        label="Publish"
+        onPress={onPublishPressed}
+        style={styles.button}
       />
     </KeyboardAvoidingScreenBackground>
   );
