54a15c51635e6ab27a9983238cf272dd625488be
Julian Tigler <High5Apps@gmail.com>
2023-11-15 11:52:28 +0200

    Add fetchNextPageOfBallots to useBallots

--- app/model/Ballots.ts
+++ app/model/Ballots.ts
@@ -5,6 +5,9 @@ import { useUserContext } from './UserContext';
 import { isCurrentUserData, isDefined } from './types';
 import { getIdsFrom } from './ModelCache';
 
+// Page indexing is 1-based, not 0-based
+const firstPageIndex = 1;
+
 export default function useBallots() {
   const { cacheBallots, getCachedBallot } = useBallotContext();
   const [ballotIds, setBallotIds] = useState<string[]>([]);
@@ -12,6 +15,9 @@ export default function useBallots() {
     () => ballotIds.map(getCachedBallot).filter(isDefined),
     [ballotIds, getCachedBallot],
   );
+  const [activeCutoff, setActiveCutoff] = useState<Date>(new Date());
+  const [fetchedLastPage, setFetchedLastPage] = useState<boolean>(false);
+  const [nextPageNumber, setNextPageNumber] = useState<number>(firstPageIndex);
   const [ready, setReady] = useState<boolean>(false);
 
   const { currentUser } = useUserContext();
@@ -22,18 +28,32 @@ export default function useBallots() {
     }
 
     const now = new Date();
+    setActiveCutoff(now);
+
     const activeJwt = await currentUser.createAuthToken({ scope: '*' });
     const inactiveJwt = await currentUser.createAuthToken({ scope: '*' });
     const { e2eDecryptMany } = currentUser;
     const [
       { ballots: activeBallots, errorMessage: activeErrorMessage },
-      { ballots: inactiveBallots, errorMessage: inactiveErrorMessage },
+      {
+        ballots: inactiveBallots, errorMessage: inactiveErrorMessage,
+        paginationData,
+      },
     ] = await Promise.all([
       fetchBallots({
-        activeAt: now, e2eDecryptMany, jwt: activeJwt, sort: 'active',
+        activeAt: now,
+        createdBefore: now,
+        e2eDecryptMany,
+        jwt: activeJwt,
+        sort: 'active',
       }),
       fetchBallots({
-        e2eDecryptMany, inactiveAt: now, jwt: inactiveJwt, sort: 'inactive',
+        createdBefore: now,
+        e2eDecryptMany,
+        inactiveAt: now,
+        jwt: inactiveJwt,
+        page: firstPageIndex,
+        sort: 'inactive',
       }),
     ]);
 
@@ -45,11 +65,59 @@ export default function useBallots() {
       throw new Error(inactiveErrorMessage);
     }
 
+    const hasNextPage = paginationData && paginationData.nextPage !== null;
+    const result = { hasNextPage };
+
     const fetchedBallots = [...activeBallots!, ...inactiveBallots!];
     cacheBallots(fetchedBallots);
     setBallotIds(getIdsFrom(fetchedBallots));
+    setNextPageNumber(firstPageIndex + 1);
+    setFetchedLastPage(!hasNextPage);
     setReady(true);
+
+    return result;
   }
 
-  return { ballots, fetchFirstPageOfBallots, ready };
+  async function fetchNextPageOfBallots() {
+    if (!isCurrentUserData(currentUser)) {
+      throw new Error('Expected current user to be set');
+    }
+
+    const jwt = await currentUser.createAuthToken({ scope: '*' });
+    const { e2eDecryptMany } = currentUser;
+
+    const {
+      errorMessage, ballots: fetchedBallots, paginationData,
+    } = await fetchBallots({
+      createdBefore: activeCutoff,
+      inactiveAt: activeCutoff,
+      e2eDecryptMany,
+      jwt,
+      page: nextPageNumber,
+      sort: 'inactive',
+    });
+
+    if (errorMessage !== undefined) {
+      throw new Error(errorMessage);
+    }
+
+    const hasNextPage = paginationData && paginationData.nextPage !== null;
+    const result = { hasNextPage };
+
+    if (!fetchedBallots?.length) { return result; }
+
+    cacheBallots(fetchedBallots);
+    setNextPageNumber((pageNumber) => pageNumber + 1);
+    setBallotIds([...ballotIds, ...getIdsFrom(fetchedBallots)]);
+
+    return result;
+  }
+
+  return {
+    ballots,
+    fetchedLastPage,
+    fetchFirstPageOfBallots,
+    fetchNextPageOfBallots,
+    ready,
+  };
 }
