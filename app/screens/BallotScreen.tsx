990f02232d85ab9590a3e9a7f836dd52f531defd
Julian Tigler <High5Apps@gmail.com>
2023-11-19 13:53:05 +0200

    Add question and details sections to BallotScreen
    - Show error message if ballotId isn't in cache for some reason

--- app/screens/BallotScreen.tsx
+++ app/screens/BallotScreen.tsx
@@ -1,9 +1,60 @@
 import React from 'react';
-import PlaceholderScreen from './PlaceholderScreen';
+import { StyleSheet, Text } from 'react-native';
 import type { BallotScreenProps } from '../navigation';
+import { ScreenBackground } from '../components';
+import useTheme from '../Theme';
+import { GENERIC_ERROR_MESSAGE, useBallots } from '../model';
+
+const useStyles = () => {
+  const { colors, font, spacing } = useTheme();
+
+  const styles = StyleSheet.create({
+    details: {
+      color: colors.labelSecondary,
+    },
+    error: {
+      color: colors.error,
+      marginTop: spacing.m,
+      textAlign: 'center',
+    },
+    question: {
+      fontFamily: font.weights.semiBold,
+      marginTop: spacing.m,
+    },
+    text: {
+      color: colors.label,
+      flexShrink: 1,
+      fontSize: font.sizes.body,
+      fontFamily: font.weights.regular,
+      marginHorizontal: spacing.m,
+    },
+  });
+
+  return { styles };
+};
 
 export default function BallotScreen({ route }: BallotScreenProps) {
-  const { name, params: { ballotId } } = route;
-  console.log({ ballotId });
-  return <PlaceholderScreen name={name} />;
+  const { params: { ballotId } } = route;
+
+  const { getCachedBallot } = useBallots();
+  const ballot = getCachedBallot(ballotId);
+
+  const { styles } = useStyles();
+
+  if (!ballot) {
+    return (
+      <ScreenBackground>
+        <Text style={[styles.text, styles.error]}>{GENERIC_ERROR_MESSAGE}</Text>
+      </ScreenBackground>
+    );
+  }
+
+  return (
+    <ScreenBackground>
+      <Text style={[styles.text, styles.question]}>{ballot.question}</Text>
+      <Text style={[styles.text, styles.details]}>
+        Responses will be anonymous
+      </Text>
+    </ScreenBackground>
+  );
 }
