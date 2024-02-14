4c354050fe73d6759b01913974757505db080f48
Julian Tigler <High5Apps@gmail.com>
2024-02-14 22:36:40 +0200

    Re-add OfficeDuties edited for brevity

--- app/model/index.ts
+++ app/model/index.ts
@@ -20,6 +20,7 @@ export { default as JWT } from './JWT';
 export { default as getMessageAge } from './MessageAge';
 export { default as NewOrgSteps } from './NewOrgSteps';
 export { default as useOffices, addMetadata } from './Offices';
+export { default as OFFICE_DUTIES } from './OfficeDuties';
 export { default as getCircleColors } from './OrgScreenCircleColors';
 export { default as usePersistentValue } from './PersistentValue';
 export { default as usePosts } from './Posts';
