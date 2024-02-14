08e159c0fecdab5ca2e736e436df7334604f80ad
Julian Tigler <High5Apps@gmail.com>
2024-02-14 22:56:20 +0200

    Covert OFFICE_DUTIES from array to object

--- app/model/OfficeDuties.ts
+++ app/model/OfficeDuties.ts
@@ -1,53 +1,33 @@
-import { OfficeDuty } from './types';
-
-const OFFICE_DUTIES: OfficeDuty[] = [
-  {
-    category: 'president',
-    duties: [
-      'Leads the Org',
-      'Represents the Org in external matters',
-      'Works with the other officers to ensure the well-being of the Org',
-      'Presides over all meetings',
-      'Decides questions of order',
-    ],
-  },
-  {
-    category: 'vice_president',
-    duties: [
-      'Supports the president',
-      "Acts as president in the president's absense",
-    ],
-  },
-  {
-    category: 'secretary',
-    duties: [
-      'Records and reports minutes on the proceedings of all meetings',
-      "Keeps the Org's records up-to-date",
-    ],
-  },
-  {
-    category: 'treasurer',
-    duties: [
-      'Handles the financial matters of the Org',
-      'Approves spending and reimbursements',
-    ],
-  },
-  {
-    category: 'steward',
-    duties: [
-      'Guides new members',
-      'Advises members during investigatory interviews by management',
-      'Listens to members and raises their concerns to other officers',
-      'Promotes solidarity among all members',
-    ],
-  },
-  {
-    category: 'trustee',
-    duties: [
-      'Audits the bookkeeping of the Org periodically',
-      'Checks the integrity of the treasurer',
-    ],
-  },
-];
+const OFFICE_DUTIES = {
+  president: [
+    'Leads the Org',
+    'Represents the Org in external matters',
+    'Works with the other officers to ensure the well-being of the Org',
+    'Presides over all meetings',
+    'Decides questions of order',
+  ],
+  vice_president: [
+    'Supports the president',
+    "Acts as president in the president's absense",
+  ],
+  secretary: [
+    'Records and reports minutes on the proceedings of all meetings',
+    "Keeps the Org's records up-to-date",
+  ],
+  treasurer: [
+    'Handles the financial matters of the Org',
+    'Approves spending and reimbursements',
+  ],
+  steward: [
+    'Guides new members',
+    'Advises members during investigatory interviews by management',
+    'Listens to members and raises their concerns to other officers',
+    'Promotes solidarity among all members',
+  ],
+  trustee: [
+    'Audits the bookkeeping of the Org periodically',
+    'Checks the integrity of the treasurer',
+  ],
+};
 
 export default OFFICE_DUTIES;
