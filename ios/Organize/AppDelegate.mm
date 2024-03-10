c7de92b9394f920d1f9fd8a3253b14f62bcb4db6
Julian Tigler <High5Apps@gmail.com>
2024-03-10 17:48:54 +0200

    Use primary color for UIAlertController buttons
    - Still uses red for destructive actions
    - https://stackoverflow.com/a/43954654/2421313

--- ios/Organize/AppDelegate.mm
+++ ios/Organize/AppDelegate.mm
@@ -11,6 +11,8 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  
+  [UIView appearanceWhenContainedInInstancesOfClasses: @[UIAlertController.self]].tintColor = [UIColor colorNamed:@"Primary"];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
