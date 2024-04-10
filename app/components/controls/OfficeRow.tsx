28e63ae268cf5c5990e12c8ad8efd1d425f5baff
Julian Tigler <High5Apps@gmail.com>
2024-04-10 12:43:55 +0300

    Add optional selected prop to OfficeRow
    - When selected is included, a checkbox is added to the start of the row and the disclosure icon is hidden

--- app/components/controls/OfficeRow.tsx
+++ app/components/controls/OfficeRow.tsx
@@ -1,31 +1,73 @@
 import React from 'react';
-import { StyleProp, ViewStyle } from 'react-native';
+import {
+  StyleProp, StyleSheet, TouchableHighlight, View, ViewStyle,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Office } from '../../model';
 import IconRow from './IconRow';
+import useTheme from '../../Theme';
+
+const useStyles = () => {
+  const { colors, spacing, sizes } = useTheme();
+
+  const styles = StyleSheet.create({
+    container: {
+      alignItems: 'center',
+      backgroundColor: colors.fill,
+      flex: 1,
+      flexDirection: 'row',
+    },
+    icon: {
+      color: colors.primary,
+      fontSize: sizes.mediumIcon,
+      paddingStart: spacing.m,
+    },
+  });
+
+  return { colors, styles };
+};
 
 type Props = {
   item: Office;
   onPress?: () => void;
+  selected?: boolean;
   style?: StyleProp<ViewStyle>;
   textButtonLabel?: string;
 };
 
 export default function OfficeRow({
-  item: { iconName, title }, onPress, style, textButtonLabel,
+  item: { iconName, title }, onPress, selected, style, textButtonLabel,
 }: Props) {
+  const { colors, styles } = useStyles();
+  const hasCheckBox = selected !== undefined;
   return (
-    <IconRow
-      iconName={iconName}
-      onPress={onPress}
-      style={style}
-      textButtonLabel={textButtonLabel}
-      title={title}
-    />
+    <TouchableHighlight
+      onPress={hasCheckBox ? onPress : undefined}
+      underlayColor={colors.label}
+    >
+      <View style={styles.container}>
+        {hasCheckBox && (
+          <Icon
+            name={selected ? 'check-box' : 'check-box-outline-blank'}
+            style={styles.icon}
+          />
+        )}
+        <IconRow
+          hideDisclosureIcon={hasCheckBox}
+          iconName={iconName}
+          onPress={hasCheckBox ? undefined : onPress}
+          style={style}
+          textButtonLabel={textButtonLabel}
+          title={title}
+        />
+      </View>
+    </TouchableHighlight>
   );
 }
 
 OfficeRow.defaultProps = {
   onPress: undefined,
+  selected: undefined,
   style: {},
   textButtonLabel: undefined,
 };
