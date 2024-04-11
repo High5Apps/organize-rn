fcf828dddad99d1346d8bb2360bb44b12591f271
Julian Tigler <High5Apps@gmail.com>
2024-04-11 17:56:02 +0300

    Add updatePermission to usePermission return

--- app/model/Permission.ts
+++ app/model/Permission.ts
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import isEqual from 'react-fast-compare';
-import { Permission, PermissionScope } from './types';
+import { OfficeCategory, Permission, PermissionScope } from './types';
 import useCurrentUser from './CurrentUser';
-import { fetchPermission } from '../networking';
+import { createPermission, fetchPermission } from '../networking';
+import { getOffice } from './Offices';
 
 type Props = {
   scope: PermissionScope;
@@ -12,7 +13,7 @@ export default function usePermission({ scope }: Props) {
   const [permission, setPermission] = useState<Permission>();
   const { currentUser } = useCurrentUser();
 
-  async function refreshPermission() {
+  const refreshPermission = useCallback(async () => {
     if (!currentUser) { throw new Error('Expected currentUser to be set'); }
 
     const jwt = await currentUser.createAuthToken({ scope: '*' });
@@ -27,7 +28,28 @@ export default function usePermission({ scope }: Props) {
     if (!isEqual(permission, fetchedPermission)) {
       setPermission(fetchedPermission);
     }
-  }
+  }, [currentUser, permission]);
 
-  return { permission, refreshPermission };
+  const updatePermission = useCallback(
+    async ({ offices }: { offices: OfficeCategory[] }) => {
+      if (!permission) { throw new Error('Expected permission to be set'); }
+      if (!currentUser) { throw new Error('Expected currentUser to be set'); }
+
+      const jwt = await currentUser.createAuthToken({ scope: '*' });
+      const { errorMessage } = await createPermission({ jwt, scope, offices });
+
+      if (errorMessage !== undefined) {
+        throw new Error(errorMessage);
+      }
+
+      const updatedPermission = { ...permission };
+      updatedPermission.data = {
+        offices: offices.map((o) => getOffice(o)),
+      };
+      setPermission(updatedPermission);
+    },
+    [currentUser, permission],
+  );
+
+  return { permission, refreshPermission, updatePermission };
 }
