06433451b17d984b2a490d2399fbecd54203e282
Julian Tigler <High5Apps@gmail.com>
2024-04-17 14:23:04 +0300

    Add updateOrg to useOrg return

--- app/model/Org.ts
+++ app/model/Org.ts
@@ -1,8 +1,13 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import isEqual from 'react-fast-compare';
-import { fetchOrg } from '../networking';
+import { fetchOrg, updateOrg as updateBackendOrg } from '../networking';
 import { Org, OrgGraph } from './types';
 import useCurrentUser from './CurrentUser';
+
+type UpdateProps = {
+  memberDefinition?: string;
+  name?: string;
+};
 
 export default function useOrg() {
   const [org, setOrg] = useState<Org>();
@@ -10,7 +15,7 @@ export default function useOrg() {
 
   const { currentUser, setCurrentUser } = useCurrentUser();
 
-  async function refreshOrg() {
+  const refreshOrg = useCallback(async () => {
     if (!currentUser) { throw new Error('Expected currentUser to be set'); }
 
     const jwt = await currentUser.createAuthToken({ scope: '*' });
@@ -31,12 +36,36 @@ export default function useOrg() {
       setOrg(fetchedOrg);
       setCurrentUser({ ...currentUser, org: fetchedOrg });
     }
-  }
+  }, [currentUser, org, orgGraph]);
+
+  const updateOrg = useCallback(async ({ memberDefinition, name }: UpdateProps) => {
+    if (!currentUser || !org) {
+      throw new Error('Expected currentUser to be set');
+    }
+
+    const jwt = await currentUser.createAuthToken({ scope: '*' });
+    const { e2eEncrypt } = currentUser;
+    const { errorMessage } = await updateBackendOrg({
+      e2eEncrypt, jwt, memberDefinition, name,
+    });
+
+    if (errorMessage !== undefined) {
+      throw new Error(errorMessage);
+    }
+
+    const updatedOrg = {
+      ...org,
+      memberDefinition: memberDefinition ?? org.memberDefinition,
+      name: name ?? org.name,
+    };
+    setOrg(updatedOrg);
+    setCurrentUser({ ...currentUser, org: updatedOrg });
+  }, [currentUser, org]);
 
   const nodeCount = (orgGraph?.userIds ?? []).length;
   const hasMultipleNodes = nodeCount > 1;
 
   return {
-    org, orgGraph, hasMultipleNodes, refreshOrg,
+    org, orgGraph, hasMultipleNodes, refreshOrg, updateOrg,
   };
 }
