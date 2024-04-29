High5Apps/organize-rn

commit 855e7986a562460d7e8c7e6ae3bc1da1367f98cf

Fix currentUser wasn't always cached in UserContext
- This caused cache misses when:
  - Tapping a (non-officer) Me row in the NotableUserList
  - Tapping a (non-officer) Me node in OrgGraph
- The root cause was a false assumption that current user would always hit from getCachedUser
  - This assumption should now be true

app/model/CurrentUser.ts
@@ -1,8 +1,10 @@
-import { Dispatch, SetStateAction, useMemo } from 'react';
+import {
+  Dispatch, SetStateAction, useEffect, useMemo,
+} from 'react';
 import isEqual from 'react-fast-compare';
 import { storeCurrentUserData } from './CurrentUserDataStorage';
 import CurrentUserBase from './CurrentUserBase';
-import { useCurrentUserDataContext } from '../context';
+import { useCurrentUserDataContext, useUserContext } from '../context';
 import { Keys } from './keys';
 import {
   CurrentUserData, E2EDecryptor, E2EMultiDecryptor, E2EMultiEncryptor, User,
@@ -118,10 +120,18 @@ export type CurrentUserType = ReturnType<typeof CurrentUser>;
 
 export default function useCurrentUser() {
   const { currentUserData, setCurrentUserData } = useCurrentUserDataContext();
+  const { cacheUser } = useUserContext();
 
   const currentUser = useMemo(() => {
     if (!currentUserData) { return null; }
     return CurrentUser(currentUserData, setCurrentUserData);
+  }, [currentUserData]);
+
+  useEffect(() => {
+    const user = currentUser?.user();
+    if (user) {
+      cacheUser(user);
+    }
   }, [currentUserData]);
 
   return { currentUser, setCurrentUser: setCurrentUserData };

commit 1a72e23adcb5b4240aa47deb52d1e56c239356ef

Fix cacheUser was called for currentUser with every useCurrentUser call
- Now cacheUser is only called in CurrentUser.update
- This commit breaks the assumption that the currentUser will never miss the cache with getCachedUser. However, the only place where getCachedUser is currently called is in SelectedUser. Normally the current user is used from useCurrentUser. I think SelectedUser is an unusual case, and it will always be called after CurrentUser.update is called via OrgScreen.onRefresh. As such, I don't think it's worth introducing much complexity to accommodate this special case
- Newly broken assumption was introduced in https://github.com/High5Apps/organize-rn/commit/855e798
- I also considered calling cacheUser near storeCurrentUserData in CurrentUserDataStorage or in a useEffect in CurrentUserDataContext, but neither worked because CurrentUser context is initialized before User context, so calling cacheUser no-op'd

app/model/CurrentUser.ts
@@ -1,6 +1,4 @@
-import {
-  Dispatch, SetStateAction, useEffect, useMemo,
-} from 'react';
+import { Dispatch, SetStateAction, useMemo } from 'react';
 import isEqual from 'react-fast-compare';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { storeCurrentUserData } from './CurrentUserDataStorage';
@@ -15,6 +13,7 @@ import { getUser } from '../networking';
 export function CurrentUser(
   currentUserData: CurrentUserData,
   setCurrentUserData: Dispatch<SetStateAction<CurrentUserData | null>>,
+  cacheUser: (user: User) => void,
 ) {
   const {
     authenticationKeyId, connectionCount, encryptedGroupKey, id, joinedAt,
@@ -94,6 +93,8 @@ export function CurrentUser(
       throw new Error(errorMessage);
     }
 
+    cacheUser(fetchedUser);
+
     if (!isEqual(user(), fetchedUser)) {
       setCurrentUserData((previousCurrentUserData) => {
         if (previousCurrentUserData === null) { return null; }
@@ -125,14 +126,7 @@ export default function useCurrentUser() {
 
   const currentUser = useMemo(() => {
     if (!currentUserData) { return null; }
-    return CurrentUser(currentUserData, setCurrentUserData);
-  }, [currentUserData]);
-
-  useEffect(() => {
-    const user = currentUser?.user();
-    if (user) {
-      cacheUser(user);
-    }
+    return CurrentUser(currentUserData, setCurrentUserData, cacheUser);
   }, [currentUserData]);
 
   return { currentUser, setCurrentUser: setCurrentUserData };

__tests__/model/CurrentUser.test.tsx
@@ -16,6 +16,7 @@ jest.mock('../../app/model/CurrentUserDataStorage');
 const mockStoreCurrentUserData = storeCurrentUserData as jest.Mock;
 
 const mockSetCurrentUserData = jest.fn();
+const mockCacheUser = jest.fn();
 
 describe('useCurrentUser', () => {
   describe('logOut', () => {
@@ -23,6 +24,7 @@ describe('useCurrentUser', () => {
       const mockCurrentUser = CurrentUser(
         fakeCurrentUserData,
         mockSetCurrentUserData,
+        mockCacheUser,
       );
       await mockCurrentUser.logOut();
     });

__tests__/model/QRCodeData.test.ts
@@ -19,16 +19,14 @@ mockCurrentUser.mockReturnValue({
   createAuthToken: mockCreateAuthToken,
   decryptGroupKey: mockDecryptGroupKey,
 });
+const currentUser = CurrentUser(fakeCurrentUserData, () => null, () => null);
+const formatter = QRCodeDataFormatter({ currentTime, currentUser });
 
 const consoleWarnSpy = jest.spyOn(global.console, 'warn').mockImplementation();
-
 describe('toString', () => {
   let formattedString: string;
 
   beforeEach(async () => {
-    const formatter = QRCodeDataFormatter({
-      currentTime, currentUser: CurrentUser(fakeCurrentUserData, () => null),
-    });
     formattedString = await formatter.toString();
   });
 
@@ -71,9 +69,6 @@ describe('parse', () => {
 
   beforeAll(async () => {
     consoleWarnSpy.mockClear();
-    const formatter = QRCodeDataFormatter({
-      currentTime, currentUser: CurrentUser(fakeCurrentUserData, () => null),
-    });
     formattedString = await formatter.toString();
     value = QRCodeDataParser({ input: formattedString }).parse()!;
     expect(consoleWarnSpy).not.toBeCalled();