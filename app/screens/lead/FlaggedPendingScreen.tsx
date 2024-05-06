77098b62f523a638a8ac95b8e2f7869b639741fc
Julian Tigler <High5Apps@gmail.com>
2024-05-06 14:41:56 +0300

    Navigate to PostScreen on FlaggedItemRow tap for post

--- app/screens/lead/FlaggedPendingScreen.tsx
+++ app/screens/lead/FlaggedPendingScreen.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FlaggedItemList, ScreenBackground } from '../../components';
+import type { FlaggedPendingScreenProps } from '../../navigation';
+import { FlaggedItem } from '../../model';
 
-export default function FlaggedPendingScreen() {
+export default function FlaggedPendingScreen({
+  navigation,
+}: FlaggedPendingScreenProps) {
+  const onItemPress = useCallback((flaggedItem: FlaggedItem) => {
+    const { category, id } = flaggedItem;
+
+    if (category === 'post') {
+      navigation.navigate('DiscussStack', {
+        screen: 'Post', initial: false, params: { postId: id },
+      });
+    } else {
+      console.log({ flaggedItem });
+    }
+  }, [navigation]);
+
   return (
     <ScreenBackground>
-      <FlaggedItemList onItemPress={console.log} />
+      <FlaggedItemList onItemPress={onItemPress} />
     </ScreenBackground>
   );
 }
