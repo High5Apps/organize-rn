02d32be25b93de5eb044a84b3239301c996d39cf
Julian Tigler <High5Apps@gmail.com>
2024-05-16 17:32:26 +0300

    Add allow, block, and undo buttons to FlagReportRow
    - Undo is shown when moderationEvent is non-nil

--- app/components/controls/FlagReportRow.tsx
+++ app/components/controls/FlagReportRow.tsx
@@ -6,6 +6,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import { FlagReport, getFlagIcon, truncateText } from '../../model';
 import useTheme from '../../Theme';
 import { DisclosureIcon } from '../views';
+import SecondaryButton from './SecondaryButton';
 
 // This matches the max length for ballots and posts, but not comments, which is
 // much longer.
@@ -17,13 +18,24 @@ const useStyles = () => {
   } = useTheme();
 
   const styles = StyleSheet.create({
+    button: {
+      paddingHorizontal: spacing.s,
+    },
+    buttonRow: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      columnGap: spacing.m,
+    },
     container: {
       backgroundColor: colors.fill,
-      gap: spacing.s,
-      padding: spacing.m,
+      paddingTop: spacing.m,
+      paddingStart: spacing.m,
 
       // Without this, it seems like there's more space at end vs. start
       paddingEnd: spacing.s,
+    },
+    containerTop: {
+      gap: spacing.s,
     },
     rowIcon: {
       color: colors.labelSecondary,
@@ -70,7 +82,9 @@ type Props = {
 };
 
 export default function FlagReportRow({ item, onPress }: Props) {
-  const { category, creator, flagCount } = item;
+  const {
+    category, creator, flagCount, moderationEvent,
+  } = item;
 
   const title = useMemo(
     () => truncateText({ maxLength: MAX_TITLE_LENGTH, text: item.title })
@@ -87,19 +101,50 @@ export default function FlagReportRow({ item, onPress }: Props) {
       underlayColor={colors.label}
     >
       <View style={styles.container}>
-        <View style={styles.rowTitle}>
-          <Icon
-            name={getFlagIcon(category)}
-            style={[styles.rowIcon, styles.rowIconPrimary]}
-          />
-          <Text style={styles.rowTitleText}>{title}</Text>
-          <DisclosureIcon />
+        <View style={styles.containerTop}>
+          <View style={styles.rowTitle}>
+            <Icon
+              name={getFlagIcon(category)}
+              style={[styles.rowIcon, styles.rowIconPrimary]}
+            />
+            <Text style={styles.rowTitleText}>{title}</Text>
+            <DisclosureIcon />
+          </View>
+          <View style={styles.rowSubtitle}>
+            <Icon name="flag" style={styles.rowIcon} />
+            <Text style={styles.rowSubtitleText}>{flagCount}</Text>
+            <Icon name="edit-square" style={styles.rowIcon} />
+            <Text style={styles.rowSubtitleText}>{creator.pseudonym}</Text>
+          </View>
+          <View style={styles.rowSubtitle}>
+            <Icon name="gavel" style={styles.rowIcon} />
+            <Text style={styles.rowSubtitleText}>Pending</Text>
+          </View>
         </View>
-        <View style={styles.rowSubtitle}>
-          <Icon name="flag" style={styles.rowIcon} />
-          <Text style={styles.rowSubtitleText}>{flagCount}</Text>
-          <Icon name="edit-square" style={styles.rowIcon} />
-          <Text style={styles.rowSubtitleText}>{creator.pseudonym}</Text>
+        <View style={styles.buttonRow}>
+          {moderationEvent ? (
+            <SecondaryButton
+              iconName="restart-alt"
+              label="Undo"
+              onPress={() => console.log('undo')}
+              style={styles.button}
+            />
+          ) : (
+            <>
+              <SecondaryButton
+                iconName="check"
+                label="Allow"
+                onPress={() => console.log('allow')}
+                style={styles.button}
+              />
+              <SecondaryButton
+                iconName="block"
+                label="Block"
+                onPress={() => console.log('block')}
+                style={styles.button}
+              />
+            </>
+          )}
         </View>
       </View>
     </TouchableHighlight>
