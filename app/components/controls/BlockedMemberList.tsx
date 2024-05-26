148fa03a84f3cecc69b54ac5a69864490c3b5c30
Julian Tigler <High5Apps@gmail.com>
2024-05-26 17:21:54 +0300

    Add ListEmptyComponent to BlockedMemberList

--- app/components/controls/BlockedMemberList.tsx
+++ app/components/controls/BlockedMemberList.tsx
@@ -1,13 +1,15 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { FlatList, ListRenderItem } from 'react-native';
 import { ModerationEvent, useModerationEvents } from '../../model';
 import BlockedMemberRow from './BlockedMemberRow';
-import { ItemSeparator } from '../views';
+import { ItemSeparator, ListEmptyMessage } from '../views';
 import { usePullToRefresh } from '../hooks';
+
+const LIST_EMPTY_MESSAGE = "Blocking members prevents them from accessing your Org.\n\nYou Org hasn't blocked any members.\n\nIf you need to block someone, tap the button below to get started.";
 
 export default function BlockedMemberList() {
   const {
-    fetchFirstPageOfModerationEvents, moderationEvents,
+    fetchFirstPageOfModerationEvents, moderationEvents, ready,
   } = useModerationEvents({
     actions: ['block'], active: true, moderatableType: 'User',
   });
@@ -16,6 +18,10 @@ export default function BlockedMemberList() {
     onRefresh: fetchFirstPageOfModerationEvents,
     refreshOnMount: true,
   });
+
+  const ListEmptyComponent = useMemo(() => (
+    <ListEmptyMessage asteriskDelimitedMessage={LIST_EMPTY_MESSAGE} />
+  ), []);
 
   const renderItem: ListRenderItem<ModerationEvent> = useCallback(
     ({ item }) => <BlockedMemberRow item={item} />,
@@ -27,6 +33,7 @@ export default function BlockedMemberList() {
       data={moderationEvents}
       ItemSeparatorComponent={ItemSeparator}
       keyboardShouldPersistTaps="handled"
+      ListEmptyComponent={ready ? ListEmptyComponent : null}
       ListHeaderComponent={ListHeaderComponent}
       refreshControl={refreshControl}
       refreshing={refreshing}
