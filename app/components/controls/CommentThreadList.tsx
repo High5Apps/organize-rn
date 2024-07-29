2b5171b6d1c5b9a591a7fabaf83b1ad0994db6df
Julian Tigler <High5Apps@gmail.com>
2024-07-29 10:43:20 -0700

    Add "View Full Discussion" button to CommentThreadScreen

--- app/components/controls/CommentThreadList.tsx
+++ app/components/controls/CommentThreadList.tsx
@@ -1,20 +1,49 @@
 import React, { useCallback } from 'react';
-import { FlatList, ListRenderItem } from 'react-native';
+import { FlatList, ListRenderItem, StyleSheet } from 'react-native';
 import { Comment, useCommentThread } from '../../model';
 import { usePullToRefresh } from '../hooks';
 import CommentRow from './CommentRow';
 import { ItemSeparator } from '../views';
+import TextButton from './TextButton';
+import useTheme from '../../Theme';
+
+const useStyles = () => {
+  const { spacing } = useTheme();
+
+  const styles = StyleSheet.create({
+    viewPostButton: {
+      padding: spacing.m,
+    },
+  });
+
+  return { styles };
+};
 
 type Props = {
   commentId: string;
+  onViewPostPressed?: (postId: string) => void;
 };
 
-export default function CommentThreadList({ commentId }: Props) {
+export default function CommentThreadList({
+  commentId, onViewPostPressed,
+}: Props) {
   const { cacheComment, comments, fetchThread } = useCommentThread(commentId);
+  const postId: string | undefined = comments[0]?.postId;
 
   const {
     ListHeaderComponent, refreshControl, refreshing,
   } = usePullToRefresh({ onRefresh: fetchThread, refreshOnMount: true });
+
+  const { styles } = useStyles();
+
+  const ListFooterComponent = useCallback(() => (!postId ? null : (
+    <TextButton
+      containerStyle={styles.viewPostButton}
+      onPress={() => onViewPostPressed?.(postId)}
+    >
+      View Full Discussion
+    </TextButton>
+  )), [onViewPostPressed, postId]);
 
   const renderItem: ListRenderItem<Comment> = useCallback(({ item }) => (
     <CommentRow
@@ -29,6 +58,7 @@ export default function CommentThreadList({ commentId }: Props) {
   return (
     <FlatList
       data={comments}
+      ListFooterComponent={ListFooterComponent}
       ListHeaderComponent={ListHeaderComponent}
       ItemSeparatorComponent={ItemSeparator}
       refreshControl={refreshControl}
@@ -37,3 +67,7 @@ export default function CommentThreadList({ commentId }: Props) {
     />
   );
 }
+
+CommentThreadList.defaultProps = {
+  onViewPostPressed: () => null,
+};
--- app/screens/discuss/CommentThreadScreen.tsx
+++ app/screens/discuss/CommentThreadScreen.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import type { CommentThreadScreenProps } from '../../navigation';
 import { CommentThreadList, ScreenBackground } from '../../components';
 
 export default function CommentThreadScreen({
-  route,
+  navigation, route,
 }: CommentThreadScreenProps) {
   const { commentId } = route.params;
 
+  const onViewPostPressed = useCallback((postId: string) => {
+    navigation.navigate('Post', { postId });
+  }, []);
+
   return (
     <ScreenBackground>
-      <CommentThreadList commentId={commentId} />
+      <CommentThreadList
+        commentId={commentId}
+        onViewPostPressed={onViewPostPressed}
+      />
     </ScreenBackground>
   );
 }
