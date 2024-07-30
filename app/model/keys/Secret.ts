33a1a150207201fde6de5c54133b59aad564f419
Julian Tigler <High5Apps@gmail.com>
2024-07-30 15:59:15 -0700

    Remove Secret and crypto-js
    - crypto-js is no longer maintained, and it was only being used in Secret.base64, so it seemed better to remove it completely
      - https://github.com/brix/crypto-js?tab=readme-ov-file#discontinued
    - This change partially reverts 51a7968
    - Note that it's not possible to remove react-native-get-random-values with this change, since it's still required by the uuid library to create random UUIDv4s
      - https://github.com/uuidjs/uuid/issues/375

--- app/model/keys/Secret.ts
+++
@@ -1,9 +0,0 @@
-import CryptoJS from 'crypto-js';
-
-export default function Secret() {
-  return {
-    base64: (bytes: number) => (
-      CryptoJS.lib.WordArray.random(bytes).toString(CryptoJS.enc.Base64)
-    ),
-  };
-}
