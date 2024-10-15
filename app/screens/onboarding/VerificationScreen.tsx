High5Apps/organize-rn

commit 8a6cee1a5e7abc6d0ca72983d277d59c00e08244

Upgrade react-native from 0.73.6 to 0.75.3
- Perform all changes recommended by the Upgrade Helper except:
  - yarn-related code. I tried switching from npm to yarn, but I couldn't figure out how to get yarn to import the info from the existing package-lock.json into yarn.lock. The switch from npm to yarn can always be done in the future
  - updating gradle-wrapper.jar since it is a binary file
  - https://react-native-community.github.io/upgrade-helper/?from=0.73.6&to=0.75.3&package=app.getorganize.organize&name=Organize
- Use align-deps to update and align dependencies
- Update Android studio from Iguana to Ladybug because Iguana had a max Android Gradle Plugin of 8.3.2, but 8.5.x is required
- Updated react-native-pager-view from 6.2.1 to 6.4.1 to fix Android build failure
- Updated react-native-vision-camera from 4.0.1 to 4.5.3 to fix Android build failure
- Updated react-native-vector-icons from 10.0.3 to 10.2.0 to fix Android release build error
- Removed 73 instances of defaultProps and disabled react/require-default-props in .eslintrc.js to fix warnings that defaultProps is now deprecated
  - All defaults (except undefined) were transferred without changes to argument defaults. It seems like many of these could be removed completely, but doing so seemed unwise since there could unanticipated side-effects from code that expected the default value instead of undefined
  - However, CommentList's insertedComments default of [] was changed to undefined to prevent an infinite loop that happened when navigating to a PostScreen and trying to display CommentList. I'm not sure why this happened.
- Fix VerificationScreen unexpectedly showed an inputAccessory bar above the keyboard with a "Next" button by setting TextInputRow returnKeyType to none
- Known Issues
  - `npm run ios:device:release` hangs on the install step, but this also happened before upgrading

app/screens/onboarding/VerificationScreen.tsx
@@ -104,6 +104,7 @@ export default function VerificationScreen() {
             onChangeText={setCode}
             onSubmitEditing={Keyboard.dismiss}
             placeholder="123456"
+            returnKeyType="none"
             value={code}
           />
         </View>

commit 629ff443976a22ef68bd3de6aa3ec3e84f4c6d2d

Fix returnKeyType none unavailable on iOS
- https://reactnative.dev/docs/textinput#returnkeytype
- The bug was introduced in 8a6cee1 to fix the blue Next button in a keyboard accessory which showed up during the RN75 upgrade
- Verified that the error message no longer appears in the logs and that the Next button bar does not appear

app/screens/onboarding/VerificationScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from 'react';
 import {
-  Keyboard, ScrollView, StyleSheet, Text, View,
+  Keyboard, Platform, ScrollView, StyleSheet, Text, View,
 } from 'react-native';
 import {
   HeaderText, KeyboardAvoidingScreenBackground, PrimaryButton, TextButton,
@@ -104,7 +104,7 @@ export default function VerificationScreen() {
             onChangeText={setCode}
             onSubmitEditing={Keyboard.dismiss}
             placeholder="123456"
-            returnKeyType="none"
+            returnKeyType={Platform.OS === 'android' ? 'none' : 'default'}
             value={code}
           />
         </View>