c4357c14089ca9e144e74f288185b7319384ee2c
Julian Tigler <High5Apps@gmail.com>
2025-02-21 11:30:18 -0800

    Add useUnionCardInfo to UnionCardScreen
    - Very similar to useOrgInfo in EditOrgScreen
      - https://github.com/High5Apps/organize-rn/blob/e87f3937e1992e2e1f842cd3a9286006b6b3281e/app/screens/lead/EditOrgScreen.tsx#L41

--- app/screens/connect/UnionCardScreen.tsx
+++ app/screens/connect/UnionCardScreen.tsx
@@ -1,9 +1,11 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {
   HeaderText, KeyboardAvoidingScreenBackground, PrimaryButton, TextInputRow,
+  useRequestProgress,
 } from '../../components';
 import useTheme from '../../Theme';
+import { getErrorMessage, useOrg } from '../../model';
 
 export const MAX_EMAIL_LENGTH = 100;
 export const MAX_NAME_LENGTH = 100;
@@ -40,18 +42,70 @@ const useStyles = () => {
   return { styles };
 };
 
+function useUnionCardInfo({
+  setLoading, setResult,
+}: Pick<ReturnType<typeof useRequestProgress>, 'setLoading' | 'setResult'>) {
+  const [email, setEmail] = useState<string>();
+  const [employerName, setEmployerName] = useState<string>();
+  const [name, setName] = useState<string>();
+  const [orgName, setOrgName] = useState<string>();
+  const [phone, setPhone] = useState<string>();
+  const agreement = `By tapping Sign, I authorize ${orgName ?? '<org_name>'} to represent me for the purpose of collective bargaining with ${employerName ?? '<employer_name>'}.`;
+
+  const { org, refreshOrg } = useOrg();
+  const refresh = async () => {
+    setLoading(true);
+    setResult('none');
+
+    try {
+      await refreshOrg();
+    } catch (error) {
+      const errorMessage = getErrorMessage(error);
+      setResult('error', {
+        message: `${errorMessage}\nTap to try again`,
+        onPress: refresh,
+      });
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => { refresh().catch(console.error); }, []);
+
+  useEffect(() => {
+    if (!org) { return; }
+
+    setEmployerName(org.employerName);
+    setOrgName(org.name);
+  }, [org]);
+
+  return {
+    agreement,
+    email,
+    employerName,
+    name,
+    orgName,
+    phone,
+    setEmail,
+    setEmployerName,
+    setName,
+    setPhone,
+  };
+}
+
 type InputName = 'email' | 'employerName' | 'name' | 'phone';
 
 export default function UnionCardScreen() {
-  const [email, setEmail] = useState<string>();
-  const [employerName, setEmployerName] = useState<string>();
   const [focusedInput, setFocusedInput] = useState<InputName | null>(null);
-  const [name, setName] = useState<string>();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [orgName, setOrgName] = useState<string>();
-  const [phone, setPhone] = useState<string>();
 
-  const agreement = `By tapping Sign, I authorize ${orgName ?? '<org_name>'} to represent me for the purpose of collective bargaining with ${employerName ?? '<employer_name>'}.`;
+  const {
+    loading, RequestProgress, setLoading, setResult,
+  } = useRequestProgress({ removeWhenInactive: true });
+  const {
+    agreement, email, employerName, name, orgName, phone, setEmail,
+    setEmployerName, setName, setPhone,
+  } = useUnionCardInfo({ setLoading, setResult });
+  const showForm = !!orgName;
 
   const { styles } = useStyles();
 
@@ -69,80 +123,91 @@ export default function UnionCardScreen() {
 
   return (
     <KeyboardAvoidingScreenBackground contentContainerStyle={styles.container}>
-      <View style={styles.section}>
-        <HeaderText>Name</HeaderText>
-        <TextInputRow
-          autoCapitalize="words"
-          autoComplete="name"
-          autoCorrect={false}
-          autoFocus={false}
-          focused={focusedInput === 'name'}
-          maxLength={MAX_NAME_LENGTH}
-          onChangeText={setName}
-          onFocus={() => setFocusedInput('name')}
-          onSubmitEditing={onSubmitEditing('name')}
-          placeholder="Abe Lincoln"
-          submitBehavior="submit"
-          value={name}
+      {showForm && (
+        <>
+          <View style={styles.section}>
+            <HeaderText>Name</HeaderText>
+            <TextInputRow
+              autoCapitalize="words"
+              autoComplete="name"
+              autoCorrect={false}
+              autoFocus={false}
+              editable={!loading}
+              focused={focusedInput === 'name'}
+              maxLength={MAX_NAME_LENGTH}
+              onChangeText={setName}
+              onFocus={() => setFocusedInput('name')}
+              onSubmitEditing={onSubmitEditing('name')}
+              placeholder="Abe Lincoln"
+              submitBehavior="submit"
+              value={name}
+            />
+          </View>
+          <View style={styles.section}>
+            <HeaderText>Phone</HeaderText>
+            <TextInputRow
+              autoComplete="tel"
+              autoFocus={false}
+              editable={!loading}
+              focused={focusedInput === 'phone'}
+              keyboardType="phone-pad"
+              maxLength={MAX_PHONE_LENGTH}
+              onChangeText={setPhone}
+              onSubmitEditing={onSubmitEditing('phone')}
+              placeholder="5551234567"
+              submitBehavior="submit"
+              value={phone}
+            />
+          </View>
+          <View style={styles.section}>
+            <HeaderText>Email</HeaderText>
+            <TextInputRow
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect={false}
+              autoFocus={false}
+              editable={!loading}
+              focused={focusedInput === 'email'}
+              keyboardType="email-address"
+              maxLength={MAX_EMAIL_LENGTH}
+              onChangeText={setEmail}
+              onSubmitEditing={onSubmitEditing('email')}
+              placeholder="abe.lincoln@whitehouse.gov"
+              submitBehavior="submit"
+              value={email}
+            />
+          </View>
+          <View style={styles.section}>
+            <HeaderText>Employer name</HeaderText>
+            <TextInputRow
+              autoCapitalize="words"
+              autoCorrect={false}
+              autoFocus={false}
+              editable={!loading}
+              enterKeyHint="done"
+              focused={focusedInput === 'employerName'}
+              maxLength={MAX_EMPLOYER_NAME_LENGTH}
+              onChangeText={setEmployerName}
+              onSubmitEditing={onSubmitEditing('employerName')}
+              placeholder="Acme, Inc."
+              value={employerName}
+            />
+          </View>
+          <View style={styles.section}>
+            <HeaderText>Agreement</HeaderText>
+            <Text style={styles.agreement}>{agreement}</Text>
+          </View>
+        </>
+      )}
+      <RequestProgress />
+      {showForm && (
+        <PrimaryButton
+          iconName="draw"
+          label="Sign"
+          onPress={() => console.log('sign')}
+          style={styles.button}
         />
-      </View>
-      <View style={styles.section}>
-        <HeaderText>Phone</HeaderText>
-        <TextInputRow
-          autoComplete="tel"
-          autoFocus={false}
-          focused={focusedInput === 'phone'}
-          keyboardType="phone-pad"
-          maxLength={MAX_PHONE_LENGTH}
-          onChangeText={setPhone}
-          onSubmitEditing={onSubmitEditing('phone')}
-          placeholder="5551234567"
-          submitBehavior="submit"
-          value={phone}
-        />
-      </View>
-      <View style={styles.section}>
-        <HeaderText>Email</HeaderText>
-        <TextInputRow
-          autoCapitalize="none"
-          autoComplete="email"
-          autoCorrect={false}
-          autoFocus={false}
-          focused={focusedInput === 'email'}
-          keyboardType="email-address"
-          maxLength={MAX_EMAIL_LENGTH}
-          onChangeText={setEmail}
-          onSubmitEditing={onSubmitEditing('email')}
-          placeholder="abe.lincoln@whitehouse.gov"
-          submitBehavior="submit"
-          value={email}
-        />
-      </View>
-      <View style={styles.section}>
-        <HeaderText>Employer name</HeaderText>
-        <TextInputRow
-          autoCapitalize="words"
-          autoCorrect={false}
-          autoFocus={false}
-          enterKeyHint="done"
-          focused={focusedInput === 'employerName'}
-          maxLength={MAX_EMPLOYER_NAME_LENGTH}
-          onChangeText={setEmployerName}
-          onSubmitEditing={onSubmitEditing('employerName')}
-          placeholder="Acme, Inc."
-          value={employerName}
-        />
-      </View>
-      <View style={styles.section}>
-        <HeaderText>Agreement</HeaderText>
-        <Text style={styles.agreement}>{agreement}</Text>
-      </View>
-      <PrimaryButton
-        iconName="draw"
-        label="Sign"
-        onPress={() => console.log('sign')}
-        style={styles.button}
-      />
+      )}
     </KeyboardAvoidingScreenBackground>
   );
 }
