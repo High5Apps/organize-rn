High5Apps/organize-rn

commit e3853ed61ab99d7d046469aa85b21b195e7b35ed

Fix keyboard avoidance with react-native-keyboard-controller
- It seems like react native's built-in KeyboardAvoidingView only works in limited circumstances. I checked the behavior is even broken in the rn-tester app
  - Things were mostly working before, but the EditOrgScreen's new input made this issue much more obvious
- Add dependency on react-native-keyboard-controller@1.16.3
  - It supports new arch, is actively maintained, and has a huge download count when searching https://reactnative.directory/?search=keyboard
  - Note that it does require react-native-reanimated to work
  - https://kirillzyusko.github.io/react-native-keyboard-controller
  - https://github.com/kirillzyusko/react-native-keyboard-controller
- Update KeyboardAvoidingScreenBackground to use KeyboardAwareScrollView
  - https://kirillzyusko.github.io/react-native-keyboard-controller/docs/api/components/keyboard-aware-scroll-view
  - All screens except NewOrgScreen that used KeyboardAvoidingScreenBackground also had a ScrollView as one of its children, so bringing the scroll view functionality into KeyboardAvoidingScreenBackground simplified things
  - KeyboardAwareScrollView made it possible to substantially simplify KeyboardAvoidingScreenBackground because there was no need to manually calculate the keyboardVerticalOffset anymore
    - This allowed for removing the explicit dependency on @react-navigation/elements
  - There's no need to manually add the background onPress to dismiss the keyboard on background taps because that's handled automatically by keyboardShouldPersistTaps="handled"
    - So I was able to remove the onPress prop from ScreenBackground
    - https://reactnative.dev/docs/scrollview#keyboardshouldpersisttaps
- Note that adding `flex: 1` to the contentContainerStyle breaks the keyboard avoidance for some reason, so I needed to remove it from EditOrgScreen and NewYesNoBallotScreen. But removing them didn't affect the layouts
- Use KeyboardStickyView for NewOrgNavigationBar

app/model/context/providers/Context.tsx
@@ -1,4 +1,5 @@
 import React, { PropsWithChildren } from 'react';
+import { KeyboardProvider } from 'react-native-keyboard-controller';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { CurrentUserDataContextProvider } from './CurrentUserDataContext';
 import { PostContextProvider } from './PostContext';
@@ -23,9 +24,11 @@ export default function Context({ children }: PropsWithChildren<{}>) {
                   <FlagReportContextProvider>
                     <ModerationEventContextProvider>
                       <CachedValueContextProvider>
-                        <SafeAreaProvider>
-                          {children}
-                        </SafeAreaProvider>
+                        <KeyboardProvider>
+                          <SafeAreaProvider>
+                            {children}
+                          </SafeAreaProvider>
+                        </KeyboardProvider>
                       </CachedValueContextProvider>
                     </ModerationEventContextProvider>
                   </FlagReportContextProvider>

__tests__/jestSetupMockReactNativeKeyboardController.ts
@@ -1,0 +1,4 @@
+// @ts-ignore - The library doesn't include types for the mock
+import mockKeyboardController from 'react-native-keyboard-controller/jest';
+
+jest.mock('react-native-keyboard-controller', () => mockKeyboardController);

app/components/views/KeyboardAvoidingScreenBackground.tsx
@@ -1,34 +1,12 @@
-import React, { PropsWithChildren, useEffect, useState } from 'react';
-import {
-  Keyboard, KeyboardAvoidingView, StyleProp, StyleSheet, ViewStyle,
-} from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { useHeaderHeight } from '@react-navigation/elements';
+import React, { PropsWithChildren } from 'react';
+import { StyleProp, StyleSheet, ViewStyle } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-controller';
 import ScreenBackground from './ScreenBackground';
-
-function useKeyboardVisibility() {
-  const [keyboardVisible, setKeyboardVisible] = useState(false);
-
-  useEffect(() => {
-    const showSubscription = Keyboard.addListener('keyboardDidShow', () => {
-      setKeyboardVisible(true);
-    });
-    const hideSubscription = Keyboard.addListener('keyboardDidHide', () => {
-      setKeyboardVisible(false);
-    });
-
-    return () => {
-      showSubscription.remove();
-      hideSubscription.remove();
-    };
-  }, []);
-
-  return keyboardVisible;
-}
+import useTheme from '../../Theme';
 
 const useStyles = () => {
   const styles = StyleSheet.create({
-    keyboardAvoidingView: {
+    keyboardAwareScrollView: {
       flex: 1,
     },
   });
@@ -37,34 +15,25 @@ const useStyles = () => {
 };
 
 type Props = {
-  style?: StyleProp<ViewStyle>;
-  topNavigationBarHidden?: boolean;
+  contentContainerStyle?: StyleProp<ViewStyle>;
 };
 
 export default function KeyboardAvoidingScreenBackground({
-  children, style = {}, topNavigationBarHidden = false,
+  children, contentContainerStyle,
 }: PropsWithChildren<Props>) {
   const { styles } = useStyles();
-
-  const headerHeight = useHeaderHeight();
-  const { top: topPadding } = useSafeAreaInsets();
-  const keyboardVerticalOffset = topNavigationBarHidden
-    ? topPadding + headerHeight : headerHeight;
-
-  const keyboardVisible = useKeyboardVisibility();
-
-  // Without this, the onPress prevents scroll gestures on deeper components
-  const onPress = keyboardVisible ? Keyboard.dismiss : undefined;
+  const { spacing } = useTheme();
 
   return (
-    <ScreenBackground onPress={onPress}>
-      <KeyboardAvoidingView
-        behavior="padding"
-        keyboardVerticalOffset={keyboardVerticalOffset}
-        style={[styles.keyboardAvoidingView, style]}
+    <ScreenBackground>
+      <KeyboardAwareScrollView
+        bottomOffset={spacing.m}
+        contentContainerStyle={contentContainerStyle}
+        keyboardShouldPersistTaps="handled"
+        style={styles.keyboardAwareScrollView}
       >
         {children}
-      </KeyboardAvoidingView>
+      </KeyboardAwareScrollView>
     </ScreenBackground>
   );
 }

app/components/views/ScreenBackground.tsx
@@ -1,5 +1,5 @@
 import React, { PropsWithChildren } from 'react';
-import { Pressable, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import useTheme from '../../Theme';
 
 const useStyles = () => {
@@ -10,27 +10,12 @@ const useStyles = () => {
       backgroundColor: colors.background,
       flex: 1,
     },
-    pressable: {
-      flex: 1,
-    },
   });
 
   return { styles };
 };
 
-type Props = {
-  onPress?: () => void;
-};
-
-export default function ScreenBackground({
-  children, onPress,
-}: PropsWithChildren<Props>) {
+export default function ScreenBackground({ children }: PropsWithChildren<{}>) {
   const { styles } = useStyles();
-  return (
-    <Pressable disabled={!onPress} onPress={onPress} style={styles.pressable}>
-      <View style={styles.background}>
-        {children}
-      </View>
-    </Pressable>
-  );
+  return <View style={styles.background}>{children}</View>;
 }

app/screens/discuss/NewCommentScreenBase.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import { Keyboard, ScrollView, StyleSheet } from 'react-native';
+import { Keyboard, StyleSheet } from 'react-native';
 import {
   KeyboardAvoidingScreenBackground, MultilineTextInput, PrimaryButton,
   useRequestProgress,
@@ -27,9 +27,6 @@ const useStyles = () => {
     },
     requestProgress: {
       marginHorizontal: spacing.m,
-    },
-    scrollView: {
-      flex: 1,
     },
   });
 
@@ -82,31 +79,25 @@ export default function NewCommentScreenBase({
   };
 
   return (
-    <KeyboardAvoidingScreenBackground>
-      <ScrollView
-        contentContainerStyle={styles.container}
-        keyboardShouldPersistTaps="handled"
-        style={styles.scrollView}
-      >
-        {HeaderComponent}
-        <MultilineTextInput
-          autoFocus
-          editable={!loading}
-          maxLength={MAX_COMMENT_LENGTH}
-          onChangeText={setBody}
-          placeholder="What do you think?"
-          style={styles.multilineTextInput}
-          returnKeyType="default"
-          value={maybeBody}
-        />
-        <RequestProgress style={styles.requestProgress} />
-        <PrimaryButton
-          iconName="publish"
-          label="Publish"
-          onPress={onPublishPressed}
-          style={styles.button}
-        />
-      </ScrollView>
+    <KeyboardAvoidingScreenBackground contentContainerStyle={styles.container}>
+      {HeaderComponent}
+      <MultilineTextInput
+        autoFocus
+        editable={!loading}
+        maxLength={MAX_COMMENT_LENGTH}
+        onChangeText={setBody}
+        placeholder="What do you think?"
+        style={styles.multilineTextInput}
+        returnKeyType="default"
+        value={maybeBody}
+      />
+      <RequestProgress style={styles.requestProgress} />
+      <PrimaryButton
+        iconName="publish"
+        label="Publish"
+        onPress={onPublishPressed}
+        style={styles.button}
+      />
     </KeyboardAvoidingScreenBackground>
   );
 }

app/screens/discuss/NewPostScreenBase.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import {
-  Keyboard, ScrollView, StyleSheet, TextInput, View,
+  Keyboard, StyleSheet, TextInput, View,
 } from 'react-native';
 import { useCachedValue, usePost } from '../../model';
 import type { Post, PostCategory } from '../../model';
@@ -35,9 +35,6 @@ const useStyles = () => {
     },
     requestProgress: {
       marginHorizontal: spacing.m,
-    },
-    scrollView: {
-      flex: 1,
     },
   });
 
@@ -121,52 +118,50 @@ export default function NewPostScreenBase({
 
   return (
     <KeyboardAvoidingScreenBackground>
-      <ScrollView keyboardShouldPersistTaps="handled" style={styles.scrollView}>
-        {!initialCategory && (
-          <PostCategorySelector
-            disabled={loading}
-            onSelectionChanged={setPostCategory}
-            selection={postCategory}
-          />
-        )}
-        <View style={styles.container}>
-          <TextInputRow
-            editable={!loading}
-            enablesReturnKeyAutomatically // iOS only
-            maxLength={MAX_TITLE_LENGTH}
-            onChangeText={setTitle}
-            onEndEditing={({ nativeEvent: { text } }) => setTitle(text)}
-            onSubmitEditing={({ nativeEvent: { text } }) => {
-              if (text.length) {
-                multilineTextInputRef.current?.focus();
-              }
-            }}
-            placeholder="Title"
-            // Prevents dismissing the keyboard when hitting next on Android
-            // before entering any input
-            submitBehavior="submit"
-            value={title}
-          />
-          <MultilineTextInput
-            editable={!loading}
-            maxLength={MAX_BODY_LENGTH}
-            onChangeText={setBody}
-            onEndEditing={({ nativeEvent: { text } }) => setBody(text)}
-            placeholder="Body (optional)"
-            style={styles.multilineTextInput}
-            returnKeyType="default"
-            ref={multilineTextInputRef}
-            value={body}
-          />
-          <RequestProgress style={styles.requestProgress} />
-          <PrimaryButton
-            iconName="publish"
-            label="Publish"
-            onPress={onPublishPressed}
-            style={styles.button}
-          />
-        </View>
-      </ScrollView>
+      {!initialCategory && (
+        <PostCategorySelector
+          disabled={loading}
+          onSelectionChanged={setPostCategory}
+          selection={postCategory}
+        />
+      )}
+      <View style={styles.container}>
+        <TextInputRow
+          editable={!loading}
+          enablesReturnKeyAutomatically // iOS only
+          maxLength={MAX_TITLE_LENGTH}
+          onChangeText={setTitle}
+          onEndEditing={({ nativeEvent: { text } }) => setTitle(text)}
+          onSubmitEditing={({ nativeEvent: { text } }) => {
+            if (text.length) {
+              multilineTextInputRef.current?.focus();
+            }
+          }}
+          placeholder="Title"
+          // Prevents dismissing the keyboard when hitting next on Android
+          // before entering any input
+          submitBehavior="submit"
+          value={title}
+        />
+        <MultilineTextInput
+          editable={!loading}
+          maxLength={MAX_BODY_LENGTH}
+          onChangeText={setBody}
+          onEndEditing={({ nativeEvent: { text } }) => setBody(text)}
+          placeholder="Body (optional)"
+          style={styles.multilineTextInput}
+          returnKeyType="default"
+          ref={multilineTextInputRef}
+          value={body}
+        />
+        <RequestProgress style={styles.requestProgress} />
+        <PrimaryButton
+          iconName="publish"
+          label="Publish"
+          onPress={onPublishPressed}
+          style={styles.button}
+        />
+      </View>
     </KeyboardAvoidingScreenBackground>
   );
 }

app/screens/lead/EditOrgScreen.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import {
-  Keyboard, ScrollView, StyleSheet, View,
-} from 'react-native';
+import { Keyboard, StyleSheet, View } from 'react-native';
 import {
   HeaderText, KeyboardAvoidingScreenBackground, MultilineTextInput,
   PrimaryButton, TextInputRow, useRequestProgress,
@@ -23,15 +21,11 @@ const useStyles = () => {
       paddingHorizontal: spacing.m,
     },
     container: {
-      flex: 1,
       margin: spacing.m,
       rowGap: spacing.m,
     },
     multilineTextInput: {
       height: 100,
-    },
-    scrollView: {
-      flex: 1,
     },
     section: {
       rowGap: spacing.s,
@@ -125,77 +119,71 @@ export default function EditOrgScreen() {
   const { styles } = useStyles();
 
   return (
-    <KeyboardAvoidingScreenBackground>
-      <ScrollView
-        contentContainerStyle={styles.container}
-        keyboardShouldPersistTaps="handled"
-        style={styles.scrollView}
-      >
-        {org && (
-          <>
-            <View style={styles.section}>
-              <HeaderText>Org name</HeaderText>
-              <TextInputRow
-                autoFocus={false}
-                editable={!loading}
-                enablesReturnKeyAutomatically
-                maxLength={nameStep.maxLength}
-                onChangeText={setName}
-                placeholder={namePlaceholder}
-                returnKeyType="done"
-                value={name}
-              />
-            </View>
-            <View style={styles.section}>
-              <HeaderText>Org email</HeaderText>
-              <TextInputRow
-                autoFocus={false}
-                editable={!loading}
-                enablesReturnKeyAutomatically
-                maxLength={emailStep.maxLength}
-                onChangeText={setEmail}
-                placeholder={emailPlaceholder}
-                returnKeyType="done"
-                value={email}
-              />
-            </View>
-            <View style={styles.section}>
-              <HeaderText>Org memeber definition</HeaderText>
-              <MultilineTextInput
-                editable={!loading}
-                enablesReturnKeyAutomatically
-                maxLength={memberDefinitionStep.maxLength}
-                onChangeText={setMemberDefinition}
-                placeholder={definitionPlaceholder}
-                style={styles.multilineTextInput}
-                submitBehavior="blurAndSubmit"
-                returnKeyType="done"
-                value={memberDefinition}
-              />
-            </View>
-            <View style={styles.section}>
-              <HeaderText>Employer name</HeaderText>
-              <TextInputRow
-                autoFocus={false}
-                editable={!loading}
-                enablesReturnKeyAutomatically
-                maxLength={EMPLOYER_NAME_MAX_LENGTH}
-                onChangeText={setEmployerName}
-                placeholder={EMPLOYER_NAME_PLACEHOLDER}
-                returnKeyType="done"
-                value={employerName}
-              />
-            </View>
-            <PrimaryButton
-              iconName="publish"
-              label="Publish"
-              onPress={updateOrgInfo}
-              style={styles.button}
+    <KeyboardAvoidingScreenBackground contentContainerStyle={styles.container}>
+      {org && (
+        <>
+          <View style={styles.section}>
+            <HeaderText>Org name</HeaderText>
+            <TextInputRow
+              autoFocus={false}
+              editable={!loading}
+              enablesReturnKeyAutomatically
+              maxLength={nameStep.maxLength}
+              onChangeText={setName}
+              placeholder={namePlaceholder}
+              returnKeyType="done"
+              value={name}
             />
-          </>
-        )}
-        <RequestProgress />
-      </ScrollView>
+          </View>
+          <View style={styles.section}>
+            <HeaderText>Org email</HeaderText>
+            <TextInputRow
+              autoFocus={false}
+              editable={!loading}
+              enablesReturnKeyAutomatically
+              maxLength={emailStep.maxLength}
+              onChangeText={setEmail}
+              placeholder={emailPlaceholder}
+              returnKeyType="done"
+              value={email}
+            />
+          </View>
+          <View style={styles.section}>
+            <HeaderText>Org memeber definition</HeaderText>
+            <MultilineTextInput
+              editable={!loading}
+              enablesReturnKeyAutomatically
+              maxLength={memberDefinitionStep.maxLength}
+              onChangeText={setMemberDefinition}
+              placeholder={definitionPlaceholder}
+              style={styles.multilineTextInput}
+              submitBehavior="blurAndSubmit"
+              returnKeyType="done"
+              value={memberDefinition}
+            />
+          </View>
+          <View style={styles.section}>
+            <HeaderText>Employer name</HeaderText>
+            <TextInputRow
+              autoFocus={false}
+              editable={!loading}
+              enablesReturnKeyAutomatically
+              maxLength={EMPLOYER_NAME_MAX_LENGTH}
+              onChangeText={setEmployerName}
+              placeholder={EMPLOYER_NAME_PLACEHOLDER}
+              returnKeyType="done"
+              value={employerName}
+            />
+          </View>
+          <PrimaryButton
+            iconName="publish"
+            label="Publish"
+            onPress={updateOrgInfo}
+            style={styles.button}
+          />
+        </>
+      )}
+      <RequestProgress />
     </KeyboardAvoidingScreenBackground>
   );
 }

app/screens/onboarding/NewOrg/NewOrgNavigationBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardStickyView } from 'react-native-keyboard-controller';
 import { SecondaryButton } from '../../../components';
 import { NewOrgSteps } from '../../../model';
 import useTheme from '../../../Theme';
@@ -43,26 +44,28 @@ export default function NewOrgNavigationBar({
   const { styles } = useStyles();
 
   return (
-    <View style={styles.container}>
-      <SecondaryButton
-        iconName="navigate-before"
-        label="Back"
-        onPress={backPressed}
-      />
-      <Text style={styles.currentStep}>
-        {`Step ${1 + currentStep} `}
-        <Text style={styles.totalSteps}>
-          {/* The extra step is for the review page */}
-          {`of ${1 + NewOrgSteps.length}`}
+    <KeyboardStickyView>
+      <View style={styles.container}>
+        <SecondaryButton
+          iconName="navigate-before"
+          label="Back"
+          onPress={backPressed}
+        />
+        <Text style={styles.currentStep}>
+          {`Step ${1 + currentStep} `}
+          <Text style={styles.totalSteps}>
+            {/* The extra step is for the review page */}
+            {`of ${1 + NewOrgSteps.length}`}
+          </Text>
         </Text>
-      </Text>
-      <SecondaryButton
-        disabled={nextDisabled}
-        iconName="navigate-next"
-        label="Next"
-        onPress={nextPressed}
-        reversed
-      />
-    </View>
+        <SecondaryButton
+          disabled={nextDisabled}
+          iconName="navigate-next"
+          label="Next"
+          onPress={nextPressed}
+          reversed
+        />
+      </View>
+    </KeyboardStickyView>
   );
 }

commit 84135ba71f70f684868253439ba10f62be5e11a1

Fix Android screen was sometimes squished vertically
- Between 1 in 10 and 1 in 2 launches on Samsung Galaxy S10, the bottom tab bar and the top navigation bar were both inset about 50 pixels each, causing the usable screen space to be much shorter than it could be. The background color of the buggy insets was the appropriate screen background color. It was not a problem on an iPhone 13 mini or simulator.
- I was able to narrow the issue down to being introduced in e3853ed
- After that, I knew that the only thing from that change that could possibly affect all screens was the change to the Context.tsx. So I randomly tried reversing the order of KeyboardProvider and SafeAreaProvider and I was no longer able to repro the issue, even after 20+ relaunches
- I'm genuinely not sure why this broke things or why this fixed things
- I also verified that keyboard avoidance still works, even with the context switch

app/model/context/providers/Context.tsx
@@ -24,11 +24,11 @@ export default function Context({ children }: PropsWithChildren<{}>) {
                   <FlagReportContextProvider>
                     <ModerationEventContextProvider>
                       <CachedValueContextProvider>
-                        <KeyboardProvider>
-                          <SafeAreaProvider>
+                        <SafeAreaProvider>
+                          <KeyboardProvider>
                             {children}
-                          </SafeAreaProvider>
-                        </KeyboardProvider>
+                          </KeyboardProvider>
+                        </SafeAreaProvider>
                       </CachedValueContextProvider>
                     </ModerationEventContextProvider>
                   </FlagReportContextProvider>