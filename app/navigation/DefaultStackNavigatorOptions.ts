64db243f666be0a2457a382bfdd94d062a6e94eb
Julian Tigler <High5Apps@gmail.com>
2025-04-09 11:38:38 -0700

    Fix long page titles didn't trigger shorter iOS back button
    - This issue was introduced by upgrading React Navigation from 6 to 7, when I added custom fonts to the React Navigation Theme
      - 55743c2
    - React Navigation (and really its underlying use of React Native Screens) doesn't try to convert the previous screen title into "Back" on iOS if a custom font is set, so adding a custom font in Theme broke things
      - https://reactnavigation.org/docs/native-stack-navigator/#headerbackbuttondisplaymode
      > "The space-aware behavior is disabled when:
      > ...
      > - Custom font family or size is set (e.g. with headerBackTitleStyle)"
    - So the simplest fix was to update the iOS header bar fonts to not use the custom Theme fonts
      - https://github.com/react-navigation/react-navigation/blob/df49c8a3a154fc1308a2cad71c6e2d08a2c089b7/packages/native-stack/src/views/useHeaderConfigProps.tsx#L58-L69
    - I also considered manually setting the `headerBackTitle` option to literally be "Back" for long page titles, but unfortunately this showed the word Back instead of the previous page title in the iOS back menu, which is shown when long pressing the back button. Plus it was more likely that regressions would sneak in, since I'd need to remember to manually do this for every long page title.
    - I faced the same issue previously in 482e580
      - https://github.com/react-navigation/react-navigation/issues/12048
      - https://github.com/software-mansion/react-native-screens/issues/1589
      - https://github.com/software-mansion/react-native-screens/pull/2105
    - https://reactnavigation.org/docs/themes/

--- app/navigation/DefaultStackNavigatorOptions.ts
+++ app/navigation/DefaultStackNavigatorOptions.ts
@@ -1,8 +1,16 @@
+import { Platform } from 'react-native';
+import { DefaultTheme } from '@react-navigation/native';
 import useTheme from '../Theme';
 
 export default function useDefaultStackNavigatorOptions() {
   const { colors } = useTheme();
   return {
+    ...Platform.select({
+      ios: {
+        headerBackTitleStyle: DefaultTheme.fonts.regular,
+        headerTitleStyle: DefaultTheme.fonts.bold,
+      },
+    }),
     navigationBarColor: colors.fill,
   };
 }
