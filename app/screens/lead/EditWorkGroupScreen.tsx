c5456fa11025b952f776de3f14c29601eec7e5f7
Julian Tigler <High5Apps@gmail.com>
2025-04-10 16:13:23 -0700

    Layout EditWorkGroupScreen
    - Hooking up the Publish button will happen in a follow-up commit

--- app/screens/lead/EditWorkGroupScreen.tsx
+++ app/screens/lead/EditWorkGroupScreen.tsx
@@ -1,10 +1,68 @@
-import React from 'react';
-import PlaceholderScreen from '../PlaceholderScreen';
+import React, { useCallback, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import type { EditWorkGroupScreenProps } from '../../navigation';
+import {
+  KeyboardAvoidingScreenBackground, PrimaryButton, WorkGroupForm,
+  WorkGroupFormInfo,
+} from '../../components';
+import useTheme from '../../Theme';
+
+const useStyles = () => {
+  const { sizes, spacing } = useTheme();
+
+  const buttonMargin = spacing.m;
+  const buttonBoundingBoxHeight = 2 * buttonMargin + sizes.buttonHeight;
+
+  const styles = StyleSheet.create({
+    addButton: {
+      bottom: buttonMargin,
+      end: buttonMargin,
+      height: sizes.buttonHeight,
+      paddingHorizontal: buttonMargin,
+      position: 'absolute',
+    },
+    container: {
+      flex: 1,
+    },
+    contentContainerStyle: {
+      padding: spacing.m,
+      paddingBottom: buttonBoundingBoxHeight,
+      rowGap: spacing.m,
+    },
+  });
+
+  return { styles };
+};
 
 export default function EditWorkGroupScreen({
   route,
 }: EditWorkGroupScreenProps) {
   const { workGroupId } = route.params;
-  return <PlaceholderScreen name={workGroupId} />;
+
+  const [formInfo, setFormInfo] = useState<WorkGroupFormInfo>();
+  const { department, jobTitle, shift } = formInfo ?? {};
+
+  const onPublishPressed = useCallback(() => {
+    console.log({ department, jobTitle, shift });
+  }, [department, jobTitle, shift]);
+
+  const { styles } = useStyles();
+
+  return (
+    <View style={styles.container}>
+      <KeyboardAvoidingScreenBackground
+        contentContainerStyle={styles.contentContainerStyle}
+      >
+        <WorkGroupForm onChange={setFormInfo} workGroupId={workGroupId} />
+      </KeyboardAvoidingScreenBackground>
+      {jobTitle && (
+        <PrimaryButton
+          iconName="publish"
+          label="Publish"
+          onPress={onPublishPressed}
+          style={styles.addButton}
+        />
+      )}
+    </View>
+  );
 }
