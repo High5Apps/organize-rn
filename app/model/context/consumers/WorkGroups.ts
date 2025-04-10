fa24c703ee5500016453f9305a6f0239c52334a6
Julian Tigler <High5Apps@gmail.com>
2025-04-09 17:16:53 -0700

    Fix WorkGroups were sorted by ascending member count
    - Now they're sorted by descending member count, i.e. most members first
    - Introduced in e735109

--- app/model/context/consumers/WorkGroups.ts
+++ app/model/context/consumers/WorkGroups.ts
@@ -45,13 +45,13 @@ export default function useWorkGroups() {
     }
 
     // Sort alphabetically by jobTitle, then by shift, then by department with
-    // ones lacking departments first, then by member count, then finally by id
+    // ones lacking departments first, then by most members, then finally by id
     // to ensure the sort is stable even if all other fields were equal.
     const sortedWorkGroups = fetchedWorkGroups.sort((wg1, wg2) => (
       wg1.jobTitle.localeCompare(wg2.jobTitle)
         || wg1.shift.localeCompare(wg2.shift)
         || (wg1.department ?? '').localeCompare(wg2.department ?? '')
-        || (wg1.memberCount - wg2.memberCount)
+        || (wg2.memberCount - wg1.memberCount)
         || wg1.id.localeCompare(wg2.id)
     ));
 
