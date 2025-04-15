8e1b80450097c950101520f75dc7e87afe6fe6ee
Julian Tigler <High5Apps@gmail.com>
2025-04-15 13:26:15 -0700

    Simplify CachedValueCache test to use renderHook
    - Similar to e97da09

--- __tests__/model/context/providers/caches/CachedValueCache.test.tsx
+++ __tests__/model/context/providers/caches/CachedValueCache.test.tsx
@@ -1,88 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-import { render, screen } from '@testing-library/react-native';
-import useCachedValueCache from '../../../../../app/model/context/providers/caches/CachedValueCache';
-
-type Props = {
-  cachedKeyToUpdate?: string;
-  cachedValues: number[];
-};
+import { act, renderHook } from '@testing-library/react-native';
+import useCachedValueCache
+  from '../../../../../app/model/context/providers/caches/CachedValueCache';
 
 const valueToKey = (_: number, i: number) => i.toString();
 
-function TestComponent({ cachedKeyToUpdate, cachedValues }: Props) {
-  const { getCachedValue, setCachedValue } = useCachedValueCache();
-  const [
-    cachedValueKeys, setCachedValueKeys,
-  ] = useState<string[]>(cachedValues.map(valueToKey));
-
-  useEffect(() => {
-    cachedValues.forEach((cv, i) => setCachedValue(i.toString(), cv));
-
-    if (cachedKeyToUpdate) {
-      setCachedValue(cachedKeyToUpdate, 1);
-      const updatedCachedValueKeys = [...cachedValueKeys, cachedKeyToUpdate];
-      const deduplicatedCachedValueKeys = [...new Set(updatedCachedValueKeys)];
-      setCachedValueKeys(deduplicatedCachedValueKeys);
-    }
-  }, []);
-
-  return (
-    <View>
-      { cachedValueKeys.map((key) => (
-        <Text key={key} testID={key}>{getCachedValue(key)}</Text>
-      ))}
-    </View>
-  );
-}
-
-function renderTestComponent({
-  cachedKeyToUpdate, cachedValues,
-}: Props) {
-  render((
-    <TestComponent
-      cachedKeyToUpdate={cachedKeyToUpdate}
-      cachedValues={cachedValues}
-    />
-  ));
-}
-
 describe('useCommentCache', () => {
   const cachedValues = [0, 0, 0];
-  const cachedKeyToUpdate = cachedValues.length;
 
   it('should contain initially cached values', async () => {
-    renderTestComponent({ cachedValues });
+    const { result } = renderHook(useCachedValueCache);
+    act(() => cachedValues.forEach(
+      (cv, i) => result.current.setCachedValue(i.toString(), cv),
+    ));
     cachedValues.map(valueToKey).forEach((key) => {
-      const testID = key;
-      expect(screen.getByTestId(testID)).toHaveTextContent('0');
+      expect(result.current.getCachedValue(key)).toBe(0);
     });
   });
 
   it('caching new value should not erase old values', async () => {
-    renderTestComponent({
-      cachedKeyToUpdate: cachedKeyToUpdate.toString(),
-      cachedValues,
-    });
+    const { result } = renderHook(useCachedValueCache);
+    act(() => cachedValues.forEach(
+      (cv, i) => result.current.setCachedValue(i.toString(), cv),
+    ));
+    act(() => result.current.setCachedValue(cachedValues.length.toString(), 0));
     cachedValues.map(valueToKey).forEach((key) => {
-      const testID = key;
-      expect(screen.getByTestId(testID)).toHaveTextContent('0');
+      expect(result.current.getCachedValue(key)).toBe(0);
     });
-
-    const testID = cachedKeyToUpdate.toString();
-    expect(screen.getByTestId(testID)).toHaveTextContent('1');
   });
 
   it('should update previously cached value', async () => {
-    renderTestComponent({
-      cachedKeyToUpdate: cachedKeyToUpdate.toString(),
-      cachedValues,
-    });
+    const { result } = renderHook(useCachedValueCache);
+    act(() => cachedValues.forEach(
+      (cv, i) => result.current.setCachedValue(i.toString(), cv),
+    ));
+    const keyToUpdate = (cachedValues.length - 1).toString();
+    act(() => result.current.setCachedValue(keyToUpdate, 1));
     cachedValues.map(valueToKey).forEach((key) => {
-      const testID = key;
-      const expectedValue = (testID === cachedKeyToUpdate.toString())
-        ? '1' : '0';
-      expect(screen.getByTestId(testID)).toHaveTextContent(expectedValue);
+      const expectedValue = (key === keyToUpdate) ? 1 : 0;
+      expect(result.current.getCachedValue(key)).toBe(expectedValue);
     });
   });
 });
