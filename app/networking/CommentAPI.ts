High5Apps/organize-rn

commit fb346cca315c6388b2a230a4c7561f3f34dc9fef

Sanitize whitespace from Comment in useComment
- Similar to 05b1007

app/networking/CommentAPI.ts
@@ -10,7 +10,7 @@ import {
 } from './types';
 
 type Props = {
-  body: string;
+  body?: string;
   commentId?: string;
   e2eEncrypt: E2EEncryptor;
   postId: string;
@@ -29,7 +29,7 @@ export async function createComment({
 }: Props & Authorization): Promise<Return> {
   const uri = commentId ? repliesURI(commentId) : commentsURI(postId);
 
-  const encryptedBody = await encrypt(body, e2eEncrypt);
+  const encryptedBody = body ? await encrypt(body, e2eEncrypt) : undefined;
   const response = await post({
     bodyObject: { comment: { encryptedBody } }, jwt, uri,
   });

app/model/context/consumers/Comment.ts
@@ -3,9 +3,10 @@ import { useCommentContext } from '../providers';
 import useCurrentUser from './CurrentUser';
 import { Comment, createComment as create } from '../../../networking';
 import getErrorMessage from '../../ErrorMessage';
+import { sanitizeMultilineField } from '../../formatters';
 
 type CreateProps = {
-  body: string;
+  body?: string;
   commentId?: string;
   postId: string;
 };
@@ -15,9 +16,11 @@ export default function useComment() {
   const { currentUser } = useCurrentUser();
 
   const createComment = useCallback(async ({
-    body, commentId, postId,
+    body: unsanitizedBody, commentId, postId,
   }: CreateProps) => {
     if (!currentUser) { throw new Error('Expected currentUser'); }
+
+    const body = sanitizeMultilineField(unsanitizedBody);
 
     let errorMessage: string | undefined;
     let newCommentId: string | undefined;
@@ -37,7 +40,7 @@ export default function useComment() {
       const parentComment = getCachedComment(commentId);
       const comment: Comment = {
         blockedAt: null,
-        body,
+        body: body!,
         createdAt: new Date(),
         deletedAt: null,
         depth: parentComment ? (parentComment.depth + 1) : 0,

app/screens/discuss/NewCommentScreenBase.tsx
@@ -48,7 +48,7 @@ export default function NewCommentScreenBase({
   commentId, HeaderComponent, onCommentCreated = () => {}, postId,
 }: Props) {
   const cacheKey = getCacheKey({ commentId, postId });
-  const [maybeBody, setBody] = useCachedValue<string | undefined>(cacheKey);
+  const [body, setBody] = useCachedValue<string | undefined>(cacheKey);
 
   const { styles } = useStyles();
   const {
@@ -62,13 +62,8 @@ export default function NewCommentScreenBase({
     setLoading(true);
     setResult('none');
 
-    const strippedBody = maybeBody?.trim() ?? '';
-    setBody(strippedBody);
-
     try {
-      const comment = await createComment({
-        body: strippedBody, commentId, postId,
-      });
+      const comment = await createComment({ body, commentId, postId });
       setBody(undefined);
       const message = `Successfully created ${commentId ? 'reply' : 'comment'}`;
       setResult('success', { message });
@@ -89,7 +84,7 @@ export default function NewCommentScreenBase({
         placeholder="What do you think?"
         style={styles.multilineTextInput}
         returnKeyType="default"
-        value={maybeBody}
+        value={body}
       />
       <RequestProgress style={styles.requestProgress} />
       <PrimaryButton

commit abe201329693e03f452a99f6e4e67d27aade1ae9

Fix bad error message when comment body blank
- Previously fixed in dec7e33
- Regression introduced in fb346cca
- Added comment to reduce the likelihood of another regression in the future

app/networking/CommentAPI.ts
@@ -29,7 +29,10 @@ export async function createComment({
 }: Props & Authorization): Promise<Return> {
   const uri = commentId ? repliesURI(commentId) : commentsURI(postId);
 
-  const encryptedBody = body ? await encrypt(body, e2eEncrypt) : undefined;
+  // Use null instead of undefined so the error message "Body can't be blank" is
+  // shown instead of "param is missing or the value is empty: comment"
+  const encryptedBody = body ? await encrypt(body, e2eEncrypt) : null;
+
   const response = await post({
     bodyObject: { comment: { encryptedBody } }, jwt, uri,
   });