e92770543c4a9cd8c1410cb4904ab5ac22692faf
Julian Tigler <High5Apps@gmail.com>
2025-04-23 17:12:57 -0700

    Fix flaky App.test.tsx
    - Introduced in the RN79 updgrade
       - 0cbdfa71
    - Previously the test failed about 1 out of 5 runs
    - Now the test succeeds 10 out of 10 times
    - Waiting for the actual Organize text to be on screen instead of just waiting for the screen to be truthy ensures that all async Navigation code has finished running (see error message below)
    ```
    An update to ForwardRef(NavigationContainerInner) inside a test was not wrapped in act(...).
    ```

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react-native';
+import { render, screen } from '@testing-library/react-native';
 import App from '../app/App';
 
 test('renders correctly', async () => {
   render(<App />);
-  await waitFor(() => screen);
+  const appNameTextComponent = await screen.findByText('Organize');
+  expect(appNameTextComponent).toBeOnTheScreen();
 });
